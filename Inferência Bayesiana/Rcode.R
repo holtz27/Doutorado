fe22acbcbdaee27215d1dfdeb80d854141d61f67
holtz27 <97683235+holtz27@users.noreply.github.com>
2024-05-07 21:47:06 -0300

    Update Rcode.R

--- Inferência Bayesiana/Rcode.R
+++ Inferência Bayesiana/Rcode.R
@@ -3,8 +3,9 @@ tau = 0.1
 mu = -10.0
 phi = 0.98
 s2_h = 0.1
-a1 = -1.0
 s2_a = 0.1
+
+a1 = -1.0
 T = 1e3
 
 set.seed( 8936381 )
@@ -30,9 +31,25 @@ draws = rstan::sampling(model_stan,
                         data = list(T = length( y ), 
                                     y = as.numeric( y ),
                                     a1 = 0,
-                                    lambda = 0.1),
+                                    lambda = 1.0),
                         chains = 4,
                         iter = 5e3
                         )
 # Visualizar os resultados
-print( draws )
+rstan::summary( draws )$summary[c('tau', 
+                                  'mu',
+                                  'phi',
+                                  's2_h',
+                                  's2_a'), 
+                                c('mean', 
+                                  '2.5%',
+                                  '97.5%',
+                                  'n_eff',
+                                  'Rhat')] 
+# Plots
+x = rstan::extract( draws )
+plot( x$tau, type = 'l' )
+plot( x$mu, type = 'l' )
+plot( x$phi, type = 'l' )
+plot( x$s2_h, type = 'l' )
+plot( x$s2_a, type = 'l' )
