fd93c09f04f0d857a82a54a769595c46d25f72a3
holtz27 <97683235+holtz27@users.noreply.github.com>
2024-05-08 10:35:08 -0300

    Create pc_priori.R

---
+++ Inferência Bayesiana/pc_priori.R
@@ -0,0 +1,30 @@
+### Random Walk
+T = 500
+mu = matrix(0, nrow = T, ncol = 1)
+Sigma = matrix(nrow = T, ncol = T)
+s_a = 0.25
+
+for(i in 1:T){
+  Sigma[i, seq(1, i)] = seq(1, i)
+  Sigma[seq(1, i), i] = Sigma[i, seq(1, i)]
+}
+
+#set.seed( 42 )
+rw = mvtnorm::rmvnorm(n = 1, mean = mu, sigma = s_a * s_a * Sigma)
+plot(1:T, rw, type = 'l')
+
+
+### PC Priori
+pcpriori = function(x, lambda){
+  y = 0.5 * lambda * x**(-0.5) * exp( - lambda * sqrt( x ) )
+  return( y )
+}
+lambda = 1.0
+
+x = seq(0, 1, by = 0.01)
+plot(x, 
+     dweibull(x, shape = 0.5, scale = lambda**(-2)), 
+     type = 'l', 
+     xlab = expression(sigma^{2}), ylab = 'densidade',
+     main = 'PC priori'
+)
