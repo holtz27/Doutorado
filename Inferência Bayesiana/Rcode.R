4ee6319e36c35395b68c53ff0da387fc2f003b0a
holtz27 <97683235+holtz27@users.noreply.github.com>
2024-05-08 21:24:49 -0300

    Update Rcode.R

--- Inferência Bayesiana/Rcode.R
+++ Inferência Bayesiana/Rcode.R
@@ -6,16 +6,15 @@ model_stan = rstan::stan_model(file = 'tvp_svm.stan')
 
 # Simulando dados
 tau = 0.1
-mu = -5
-phi = 0.98
-s2_h = 0.25
-s2_a = 0.25
+mu = -9
+phi = 0.985
+s2_h = 0.03
+s2_a = 0.005
 
-T = 5e2
-
-#set.seed( 8936381 )
+T = 1e3 + 1
+set.seed( 42853 )
 y = h = a = matrix(0, nrow = T, ncol = 1)
-a[1] = -1.0
+a[1] = rnorm(1, mean = -1, sd = 1)
 h[1] = mu + sqrt( s2_h / (1 - phi * phi) ) * rnorm( 1 )
 y[1] = tau + a[1] * exp( h[1] ) + exp( 0.5 * h[1] ) * rnorm( 1 )
 
@@ -26,19 +25,19 @@ for( t in 2:T ){
 }
 
 plot(y, type = 'l')
-plot(a, type = 'l')
-plot(exp(h), type = 'l')
+#plot(a, type = 'l')
+#plot(exp(h), type = 'l')
 
-
-# Smapling
+# RStan Sampling
 p = 0.01
 U = 0.2
 draws = rstan::sampling(model_stan, 
-                        data = list(T = length( y ), 
-                                    y = as.numeric( y ),
+                        data = list(T = length( y[ 1:(T-1) ] ), 
+                                    y = as.numeric( y[ 1:(T-1) ] ),
                                     lambda = -log( p ) / sqrt( U ) ),
-                        chains = 4,
-                        iter = 5e2
+                        chains = 1,
+                        iter = 5e2,
+                        cores = 4
 )
 
 # Análise numérica
@@ -48,28 +47,30 @@ round(
   3
 )
 
-# Análise gráfica
 x = rstan::extract( draws )
 draws_tau = x$tau
 draws_mu = x$mu
 draws_phi = x$phi
 draws_s2_h = x$s2_h
 draws_s2_a = x$s2_a
-Draws = matrix( draws_tau, nrow = 1 )
-Draws = rbind( Draws, draws_mu )
-Draws = rbind( Draws, draws_phi )
-Draws = rbind( Draws, draws_s2_h )
-Draws = rbind( Draws, draws_s2_a )
-trace_plots(Draws, names = c(expression(tau), 
+theta = matrix( draws_tau, nrow = 1 )
+theta = rbind( theta, draws_mu )
+theta = rbind( theta, draws_phi )
+theta = rbind( theta, draws_s2_h )
+theta = rbind( theta, draws_s2_a )
+
+# Análise gráfica
+trace_plots(theta, names = c(expression(tau), 
                              expression(mu), 
                              expression(phi), 
                              expression(sigma[h]^{2}), 
                              expression(sigma[a]^{2}))
             )
 # h
-draws_h = apply( x$h, MARGIN = 2, mean )
-h_min = apply( x$h, MARGIN = 2, quantile, probs = c(0.025, 0.975) )[1, ] 
-h_max = apply( x$h, MARGIN = 2, quantile, probs = c(0.025, 0.975) )[2, ]
+h_hmc = x$h
+draws_h = apply( h_hmc, MARGIN = 2, mean )
+h_min = apply( h_hmc, MARGIN = 2, quantile, probs = c(0.025, 0.975) )[1, ] 
+h_max = apply( h_hmc, MARGIN = 2, quantile, probs = c(0.025, 0.975) )[2, ]
 data = matrix(c(h, draws_h, h_min, h_max), ncol = 4)
 data = data.frame( data )
 data = cbind( c(1:T), data )
@@ -87,9 +88,10 @@ g0 = g0 + theme(axis.title.x = element_text(size = 26),
                 axis.text.y = element_text(size = 26))
 
 # a
-draws_a = apply( x$a, MARGIN = 2, mean )
-a_min = apply( x$a, MARGIN = 2, quantile, probs = c(0.025, 0.975) )[1, ] 
-a_max = apply( x$a, MARGIN = 2, quantile, probs = c(0.025, 0.975) )[2, ]
+a_hmc = x$a
+draws_a = apply( a_hmc, MARGIN = 2, mean )
+a_min = apply( a_hmc, MARGIN = 2, quantile, probs = c(0.025, 0.975) )[1, ] 
+a_max = apply( a_hmc, MARGIN = 2, quantile, probs = c(0.025, 0.975) )[2, ]
 data = matrix(c(a, draws_a, a_min, a_max), ncol = 4)
 data = data.frame( data )
 data = cbind( c(1:T), data )
@@ -107,4 +109,11 @@ g1 = g1 + theme(axis.title.x = element_text(size = 26),
                 axis.text.x = element_text(size = 24),
                 axis.text.y = element_text(size = 26))
 
-gridExtra::grid.arrange(g0, g1, nrow=1, widths=c(1.5, 1))
+gridExtra::grid.arrange(g0, g1, nrow = 2)
+
+
+# Evaluation VaR
+VaR_hat = VaR(h_T = h_hmc[, dim( h_hmc )[2]],
+              a_T = a_hmc[, dim( h_hmc )[2]],
+              theta_hmc = theta
+              )
