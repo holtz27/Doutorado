ff4961fad24f88a76f8d966ea17e3bb67b60aeeb
holtz27 <97683235+holtz27@users.noreply.github.com>
2024-05-10 18:44:07 -0300

    Update lambda.R

--- Inferência Bayesiana/lambda.R
+++ Inferência Bayesiana/lambda.R
@@ -3,19 +3,19 @@ x = seq(0, 0.25, by = 0.005)
 
 # high informative
 p1 = 0.01
-U1 = 0.25
+U1 = 0.1
 lambda1 = -log( p1 ) / sqrt( U1 ) 
 y1 = dweibull(x, shape = 0.5, scale = lambda1**(-2))
 
 #middle informative
 p2 = 0.1
-U2 = 0.25
+U2 = 0.1
 lambda2 = -log( p2 ) / sqrt( U2 ) 
 y2 = dweibull(x, shape = 0.5, scale = lambda2**(-2))
 
 # weakly informative
 p3 = 0.5
-U3 = 0.25
+U3 = 0.1
 lambda3 = -log( p3 ) / sqrt( U3 ) 
 y3 = dweibull(x, shape = 0.5, scale = lambda3**(-2))
 
@@ -29,7 +29,7 @@ plot(x,
 lines(x, y2, lty = 2)
 lines(x, y3, lty = 3)
 legend('topright', 
-       legend = c('(U, p) = (0.25, 0.01)', 
-                  '(U, p) = (0.25, 0.10)', 
-                  '(U, p) = (0.25, 0.50)'), 
+       legend = c(paste0('(U, p) = (', U1, ', ', p1,')' ), 
+                  paste0('(U, p) = (', U2, ', ', p2,')' ), 
+                  paste0('(U, p) = (', U3, ', ', p3,')' )), 
       lty = c(1, 2, 3), lwd = 2, bty = 'n')
