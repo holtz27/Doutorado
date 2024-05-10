e7ef93b7a9ebda40b45ec4cb8be62011d92e6c96
holtz27 <97683235+holtz27@users.noreply.github.com>
2024-05-10 18:39:42 -0300

    Create lambda.R

---
+++ Inferência Bayesiana/lambda.R
@@ -0,0 +1,35 @@
+### PC Priori
+x = seq(0, 0.25, by = 0.005)
+
+# high informative
+p1 = 0.01
+U1 = 0.25
+lambda1 = -log( p1 ) / sqrt( U1 ) 
+y1 = dweibull(x, shape = 0.5, scale = lambda1**(-2))
+
+#middle informative
+p2 = 0.1
+U2 = 0.25
+lambda2 = -log( p2 ) / sqrt( U2 ) 
+y2 = dweibull(x, shape = 0.5, scale = lambda2**(-2))
+
+# weakly informative
+p3 = 0.5
+U3 = 0.25
+lambda3 = -log( p3 ) / sqrt( U3 ) 
+y3 = dweibull(x, shape = 0.5, scale = lambda3**(-2))
+
+plot(x, 
+     y1, 
+     type = 'l',
+     ylim = c(0, 15),
+     xlab = expression(sigma[a]^{2}), ylab = 'densidade',
+     main = 'PC priori'
+)
+lines(x, y2, lty = 2)
+lines(x, y3, lty = 3)
+legend('topright', 
+       legend = c('(U, p) = (0.25, 0.01)', 
+                  '(U, p) = (0.25, 0.10)', 
+                  '(U, p) = (0.25, 0.50)'), 
+      lty = c(1, 2, 3), lwd = 2, bty = 'n')
