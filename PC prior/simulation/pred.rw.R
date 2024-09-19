90b5aedf6f0c84b1a1582b2f7c66d8da64836b76
holtz27 <97683235+holtz27@users.noreply.github.com>
2024-09-19 08:32:44 -0300

    Update pred.rw.R

--- PC prior/simulation/pred.rw.R
+++ PC prior/simulation/pred.rw.R
@@ -1,12 +1,14 @@
 pred.rw = function( a_T, xi_hmc, a.test, kernel = 'epanechnikov', seed = NULL ){
   
+  log.pred = 0
+  
   if( is.null(seed) ) seed = sample(1:1e6, 1)
   set.seed(seed)
   xi = xi_hmc
   M = length( xi )
   
   densidade_a.test = rep( 0, length( a.test ) )
-
+  
   for( h in 1:length( a.test ) ){
     # Sampling from predictive density
     a.new = rnorm(n = M, mean = a_T, sd = sqrt(xi))
@@ -16,6 +18,9 @@ pred.rw = function( a_T, xi_hmc, a.test, kernel = 'epanechnikov', seed = NULL ){
     densidade_a.test[ h ] = approx(densidade$x, densidade$y, xout = a.test[ h ])$y
     a_T = a.new
   }
-
-  return( -sum( log(densidade_a.test ) ) ) 
+  
+  x = -sum( log(densidade_a.test ) )
+  if( !is.na(x) && !is.nan(x) ) log.pred = x
+  
+  return( log.pred ) 
 }
