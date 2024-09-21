dcc63c10ffd08e6a0609eaebd48d3ae1bbd3c6b7
holtz27 <97683235+holtz27@users.noreply.github.com>
2024-09-21 15:08:45 -0300

    Update and rename lpds.R to tvp_lpds_star.R

---
+++ cobal/tvp_lpds_star.R
@@ -0,0 +1,30 @@
+tvp_lpds_star = function( h_T, a_T, theta_hmc, yobs, kernel = 'epanechnikov', seed = NULL ){
+  
+  if( is.null(seed) ) seed = sample(1:1e6, 1)
+  set.seed(seed)
+  theta = theta_hmc
+  M = dim( theta )[ 2 ]
+  
+  log.pred = 0
+  
+  # h_new = mu + phi * ( h_T - mu ) + sigma_h * eps_h
+  eps_h = rnorm( M, sd = sqrt(theta[4, ]) )
+  h_new = theta[2, ] + theta[3, ] * ( h_T - theta[2, ] ) + eps_h
+  # a_new = a_T + eps_a
+  eps_a = rnorm( M, sd = sqrt(theta[5, ]) )
+  a_new = a_T + eps_a
+  # y_new = mu + a_T * exp{ h_T } + eps_y
+  y_new = matrix( nrow = 1, ncol = M )
+  eps_y = rnorm( M, sd = exp( 0.5 * h_new ) )
+  y_new = theta[1, ] + a_new * exp( h_new ) + eps_y
+  
+  # Calcula a densidade usando a função density do R
+  densidade = density(y_new, kernel = kernel)
+  # Aproxima a densidade no ponto yobs
+  densidade_yobs = approx(densidade$x, densidade$y, xout = yobs)$y
+  
+  x = log( densidade_yobs )
+  if( !is.na(x) && !is.nan(x) ) log.pred = x
+  
+  return( log.pred ) 
+}
