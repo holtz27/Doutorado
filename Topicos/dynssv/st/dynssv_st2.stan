a9dd27c11992aa8ea79a667cdab6f3dda0547528
holtz27 <97683235+holtz27@users.noreply.github.com>
2024-10-08 14:29:19 -0300

    Add files via upload

---
+++ Topicos/dynssv/st/dynssv_st2.stan
@@ -0,0 +1,84 @@
+data {
+  int<lower=0> T;
+  vector[T] y;
+  real<lower=0> lambda1;
+  real<lower=0> lambda2;
+}
+parameters{
+  real mu_h;
+  real mu_a;
+  real<lower=0, upper=1> phiT_h;
+  real<lower=0, upper=1> phiT_a;
+  real<lower=0> s_h;
+  real<lower=0> s_a;
+  vector[T] h_std;
+  vector[T] a_std;
+  vector<lower=0>[T] W;
+  vector<lower=0>[T] U;
+  
+  real<lower=2> v;
+  real<lower=0> k;
+}
+transformed parameters{
+  real<lower=-1,upper=1> phi_h;
+  real<lower=-1,upper=1> phi_a;
+  real<lower=0> s2_h;
+  real<lower=0> s2_a;
+  vector[T] a = a_std * s_a;
+  vector[T] h = h_std * s_h;  
+  real ls_a = log( s_a );
+  phi_h = ( 2 * phiT_h - 1 );
+  phi_a = ( 2 * phiT_a - 1 );
+  s2_h = pow( s_h, 2 );
+  s2_a = pow( s_a, 2 );
+  
+  h[1] /= sqrt(1 - phi_h * phi_h);
+  a[1] /= sqrt(1 - phi_a * phi_a);
+  h += mu_h;
+  a += mu_a;
+  for (t in 2:T){
+    h[t] += phi_h * (h[t - 1] - mu_h);
+    a[t] += phi_a * (a[t - 1] - mu_a);
+  }
+  
+  vector<lower=-1, upper=1>[T] delta = a ./ sqrt(1 + square(a) );
+  real k1 = sqrt(0.5 * v) * tgamma(0.5*(v-1)) / tgamma(0.5 * v);
+  real k2 = v / (v-2);
+  vector[T] omega = 1 ./ sqrt(k2 - 2 * square( delta * k1 ) / pi()); 
+  vector[T] mean_st = - sqrt(2 / pi()) * k1 * delta .* omega;
+  vector[T] mu_t = mean_st + omega .* delta .* W .* exp(0.5 * h) ./ sqrt( U );
+  vector[T] sigma_t = omega .* sqrt(1 - square ( delta )) .* exp(0.5 * h) ./ sqrt( U );
+  
+}
+model {
+  // Prioris h
+  mu_h ~ normal( 0, sqrt(10) );
+  phiT_h ~ beta( 20, 1.5 );
+  s2_h ~ inv_gamma( 2.5, 0.025 );
+  
+  // Prioris a
+  mu_a ~ normal( 0, sqrt(10) );
+  
+  //priori1
+  //s2_a ~ weibull( 0.5, pow(lambda2, -2 ) );
+  
+  //prior2
+  s2_a ~ inv_gamma( 2.5, 0.025 );
+  
+  //prior3
+  //k ~ gamma(0.1, 0.1);
+  //s_a ~ exponential( k );
+  
+  target += - lambda1 * sqrt(1 - phi_a) - 0.5 * log(1 - phi_a);
+  
+  //tails
+  v ~ gamma(12, .8);
+  
+  // model
+  U ~ gamma(0.5 * v, 0.5 * v);
+  h_std ~ std_normal();
+  a_std ~ std_normal();
+  y ~ normal( mu_t, sigma_t );
+  target += - 0.5 * square( W );
+  
+}
--- Topicos/dynssv/st/simulacao1.R
+++ Topicos/dynssv/st/simulacao1.R
@@ -7,8 +7,12 @@ source('https://raw.githubusercontent.com/holtz27/svmsmn/refs/heads/main/source/
 localenvir = rstudioapi::getActiveDocumentContext()
 path = localenvir$path
 dir = dirname(path)
-path = paste0( dir, '/dynssv_st.stan')
-model_stan = rstan::stan_model(file = path)
+path = paste0( dir, '/dynssv_st1.stan')
+model_stan1 = rstan::stan_model(file = path)
+path = paste0( dir, '/dynssv_st2.stan')
+model_stan2 = rstan::stan_model(file = path)
+path = paste0( dir, '/dynssv_st3.stan')
+model_stan3 = rstan::stan_model(file = path)
 
 set.seed(164872)
 T = 1e3
@@ -23,14 +27,14 @@ sigma_a = 0.1
 v = 20
 
 theta_vdd = matrix(c(mu_h, phi_h, sigma_h, 
-                     mu_a, phi_a, sigma_a, 
+                     mu_a, phi_a, log(sigma_a), 
                      v), ncol = 1)
-summary = list()
-prob = matrix(0, nrow = 7, ncol = 1)
+summary1 = summary2 = summary3 = list()
+prob1 = prob2 = prob3 = matrix(0, nrow = 7, ncol = 1)
 
 M = 2
-err = matrix(0, nrow = 7, ncol = M)
-vies = reqm = matrix(0, nrow = 1, ncol = 3)
+err1 = err2 = err3= matrix(0, nrow = 7, ncol = M)
+vies1 = reqm1 = vies2 = reqm2 = vies3 = reqm3 = matrix(0, nrow = 1, ncol = 3)
 
 for(it in 1:M){
   if( it == 1 ) time = Sys.time()
@@ -52,16 +56,16 @@ for(it in 1:M){
   sigma_t = omega * sqrt(1 - ( delta )^2) * exp(0.5 * h) / sqrt( U )
   y = mu_t + sigma_t * rnorm(T)
   
-  ### Sampling
-  draws = rstan::sampling(model_stan, 
+  ### Sampling1
+  draws = rstan::sampling(model_stan1, 
                           data = list(T = length( y ), 
                                       y = as.numeric( y ),
                                       lambda1 = 1.55,
-                                      lambda2 = 15
+                                      lambda2 = 14.34
                           ),
                           chains = 1,
-                          warmup = 1e2,
-                          iter = 1e2 + 2e1,
+                          warmup = 2e2,
+                          iter = 2e2 + 2e2,
                           cores = 1
   )
   x = rstan::extract( draws )
@@ -80,32 +84,138 @@ for(it in 1:M){
   theta = rbind( theta, draws_s_a )
   theta = rbind( theta, draws_v )
   
-  summary[[ it ]] = num_analisys(draws = theta, 
-                                 names = c('mu_h', 'phi_h', 's_h', 
-                                           'mu_a', 'phi_a', 's_a', 
-                                           'v'),
-                                 digits = 4,
-                                 hdp = TRUE
+  summary1[[ it ]] = num_analisys(draws = theta, 
+                                  names = c('mu_h', 'phi_h', 's_h', 
+                                            'mu_a', 'phi_a', 'ls_a', 
+                                            'v'),
+                                  digits = 4,
+                                  hdp = TRUE
+                                  )
+  # error
+  err1[ ,it ] = matrix(summary1[[ it ]][, 1], ncol = 1) - theta_vdd
+  # prob
+  x1 = as.numeric( summary1[[ it ]][, 3] < theta_vdd )
+  x2 = as.numeric( summary1[[ it ]][, 4] > theta_vdd )
+  prob.piv = matrix( round( 0.5 * (x1 + x2), 0 ), ncol = 1)
+  prob1 = prob1 + prob.piv
+  
+  ### Sampling2
+  draws = rstan::sampling(model_stan2, 
+                          data = list(T = length( y ), 
+                                      y = as.numeric( y ),
+                                      lambda1 = 1.55,
+                                      lambda2 = 14.34
+                          ),
+                          chains = 1,
+                          warmup = 2e2,
+                          iter = 2e2 + 2e2,
+                          cores = 1
+  )
+  x = rstan::extract( draws )
+  draws_mu_h = x$mu_h
+  draws_phi_h = x$phi_h
+  draws_s_h = x$s_h
+  draws_mu_a = x$mu_a
+  draws_phi_a = x$phi_a
+  draws_s_a = x$s_a
+  draws_v = x$v
+  theta = matrix( draws_mu_h, nrow = 1 )
+  theta = rbind( theta, draws_phi_h )
+  theta = rbind( theta, draws_s_h )
+  theta = rbind( theta, draws_mu_a)
+  theta = rbind( theta, draws_phi_a )
+  theta = rbind( theta, draws_s_a )
+  theta = rbind( theta, draws_v )
+  
+  summary2[[ it ]] = num_analisys(draws = theta, 
+                                  names = c('mu_h', 'phi_h', 's_h', 
+                                            'mu_a', 'phi_a', 'ls_a', 
+                                            'v'),
+                                  digits = 4,
+                                  hdp = TRUE
   )
   # error
-  err[ ,it ] = matrix(summary[[ it ]][, 1], ncol = 1) - theta_vdd
+  err2[ ,it ] = matrix(summary2[[ it ]][, 1], ncol = 1) - theta_vdd
   # prob
-  x1 = as.numeric( summary[[ it ]][, 3] < theta_vdd )
-  x2 = as.numeric( summary[[ it ]][, 4] < theta_vdd )
+  x1 = as.numeric( summary2[[ it ]][, 3] < theta_vdd )
+  x2 = as.numeric( summary2[[ it ]][, 4] > theta_vdd )
   prob.piv = matrix( round( 0.5 * (x1 + x2), 0 ), ncol = 1)
-  prob = prob + prob.piv
+  prob2 = prob2 + prob.piv
+  
+  ### Sampling3
+  draws = rstan::sampling(model_stan3, 
+                          data = list(T = length( y ), 
+                                      y = as.numeric( y ),
+                                      lambda1 = 1.55,
+                                      lambda2 = 14.34
+                          ),
+                          chains = 1,
+                          warmup = 2e2,
+                          iter = 2e2 + 2e2,
+                          cores = 1
+  )
+  x = rstan::extract( draws )
+  draws_mu_h = x$mu_h
+  draws_phi_h = x$phi_h
+  draws_s_h = x$s_h
+  draws_mu_a = x$mu_a
+  draws_phi_a = x$phi_a
+  draws_s_a = x$s_a
+  draws_v = x$v
+  theta = matrix( draws_mu_h, nrow = 1 )
+  theta = rbind( theta, draws_phi_h )
+  theta = rbind( theta, draws_s_h )
+  theta = rbind( theta, draws_mu_a)
+  theta = rbind( theta, draws_phi_a )
+  theta = rbind( theta, draws_s_a )
+  theta = rbind( theta, draws_v )
+  
+  summary3[[ it ]] = num_analisys(draws = theta, 
+                                  names = c('mu_h', 'phi_h', 's_h', 
+                                            'mu_a', 'phi_a', 'ls_a', 
+                                            'v'),
+                                  digits = 4,
+                                  hdp = TRUE
+  )
+  # error
+  err3[ ,it ] = matrix(summary3[[ it ]][, 1], ncol = 1) - theta_vdd
+  # prob
+  x1 = as.numeric( summary1[[ it ]][, 3] < theta_vdd )
+  x2 = as.numeric( summary1[[ it ]][, 4] > theta_vdd )
+  prob.piv = matrix( round( 0.5 * (x1 + x2), 0 ), ncol = 1)
+  prob3 = prob3 + prob.piv
   
   if( it == M ) time = Sys.time() - time
 }
 
 time
-vies = matrix( apply( err, MARGIN = 1, mean ), ncol = 1 )
-reqm = matrix( apply( err^2, MARGIN = 1, mean ), ncol = 1 )
-prob = prob / M
-data = cbind( vies, reqm, prob )
-data = data.frame( data )
+
+vies1 = matrix( apply( err1, MARGIN = 1, mean ), ncol = 1 )
+reqm1 = matrix( apply( err1^2, MARGIN = 1, mean ), ncol = 1 )
+prob1 = prob1 / M
+data1 = cbind( vies1, reqm1, prob1 )
+data1 = data.frame( data1 )
+colnames( data1 ) = c('vies1', 'reqm1', 'prob.cob1')
+
+vies2 = matrix( apply( err2, MARGIN = 1, mean ), ncol = 1 )
+reqm2 = matrix( apply( err2^2, MARGIN = 1, mean ), ncol = 1 )
+prob2 = prob2 / M
+data2 = cbind( vies2, reqm2, prob2 )
+data2 = data.frame( data2 )
+colnames( data2 ) = c('vies2', 'reqm2', 'prob.cob2')
+
+vies3 = matrix( apply( err3, MARGIN = 1, mean ), ncol = 1 )
+reqm3 = matrix( apply( err3^2, MARGIN = 1, mean ), ncol = 1 )
+prob3 = prob3 / M
+data3 = cbind( vies3, reqm3, prob3 )
+colnames( data3 ) = c('vies3', 'reqm3', 'prob.cob3')
+
+data = cbind( data1, data2, data3 )
 row.names( data ) = c('mu_h', 'phi_h', 's_h', 
-                      'mu_a', 'phi_a', 's_a', 
-                      'v')
-colnames( data) = c('vies', 'reqm', 'prob.cob')
-round( data, 4 )
+                       'mu_a', 'phi_a', 'ls_a', 
+                       'v')
+Summary = list( summary1 = summary1, 
+                summary2 = summary2, 
+                summary3 = summary3 )
+
+save( data, Summary, file = paste0( dir, '/simulacao1.RData') )
