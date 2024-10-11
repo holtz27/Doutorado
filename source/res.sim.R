ebf7494355a30500b9864ecacff285c0c56b329c
holtz27 <97683235+holtz27@users.noreply.github.com>
2024-10-11 10:33:23 -0300

    Create res.sim.R

---
+++ source/res.sim.R
@@ -0,0 +1,45 @@
+res.sim = function( s , theta_vdd, med.abs = TRUE, digits = 4 ){
+  
+  T = length( s )
+  rows = dim( s[[1]] )[ 1 ]
+  if( med.abs ){
+    theta = matrix(1, nrow = rows, ncol = 1)
+  }else{
+    theta = theta_vdd
+    theta[ theta == 0 ] = 1
+  }
+  
+  err = matrix(0, nrow = rows, ncol = T) 
+  x1 = prob.cob = 0
+  errors = NULL
+  
+  for( i in 1:T ){
+    
+    if( sum( is.nan( s[[ i ]] ) + is.infinite( s[[ i ]] ) ) != 0 ){
+      errors = cbind( errors, i ) 
+      i = i + 1
+    }else{
+      
+      x1 = x1 + s[[ i ]]
+      err[ ,i ] = ( matrix(s[[ i ]][, 1], ncol = 1) - theta_vdd ) / theta
+      # prob cob
+      l1 = as.numeric( s[[ i ]][, 3] < theta_vdd )
+      l2 = as.numeric( s[[ i ]][, 4] > theta_vdd )
+      prob.piv = matrix( round( 0.5 * (l1 + l2), 0 ), ncol = 1)
+      prob.cob = prob.cob + prob.piv
+      
+    }
+    
+  } 
+  
+  Data = cbind( matrix( apply( err, MARGIN = 1, mean ), ncol = 1 ),
+                matrix( apply( err^2, MARGIN = 1, mean ), ncol = 1 ),
+                prob.cob / T )
+  row.names( Data ) = c('mu_h', 'phi_h', 's_h', 'mu_a', 'phi_a', 'ls_a', 'v')
+  colnames( Data ) = c('vies', 'reqm', 'prob.cob')
+  
+  return( list( resumo = round(x1 / T, digits), 
+                metricas = round(Data, digits),
+                errors = errors ) 
+          ) 
+}
