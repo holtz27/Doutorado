958f2e7be0508ef306f9c2eef0fceb64b23d7b46
holtz27 <97683235+holtz27@users.noreply.github.com>
2024-10-15 13:29:42 -0300

    Add files via upload

---
+++ cobal/aplicacao/pred_svm.R
@@ -0,0 +1,59 @@
+### VaR and LPDS*
+source( 'https://raw.githubusercontent.com/holtz27/Doutorado/refs/heads/main/cobal/source/svm_VaR.R' )
+source( 'https://raw.githubusercontent.com/holtz27/Doutorado/refs/heads/main/cobal/source/svm_lpds_star.R' )
+source('https://raw.githubusercontent.com/holtz27/svmsmn/refs/heads/main/source/num_analisys.R')
+
+# compiling Stan model
+dir = paste0(getwd(), '/Doutorado/Eventos/cobal/modelos/')
+path = paste0(dir, 'static.stan')
+model_stan = rstan::stan_model(file = path)
+
+horizon = 1
+VaR_hat = lpds.star = rep(0, horizon)
+summary = list()
+
+for(it in 1:horizon){
+  
+  if(it == 1) time = Sys.time()
+  
+  y.now = log.ret[ 1:(T - horizon + it - 1) ]
+  draws = rstan::sampling(model_stan, 
+                          data = list(T = length( y.now ), 
+                                      y = as.numeric( y.now )
+                          ),
+                          chains = 1,
+                          warmup = 2e3,
+                          iter = 2e3 + 1e3,
+                          cores = 1
+  )
+  x = rstan::extract(draws)
+  theta = matrix(x$b, nrow = 1)
+  theta = rbind(theta, x$mu, x$phi, x$s2_h, x$a)
+  h_hmc = x$h
+  # Summary
+  summary[[ it ]] = num_analisys(draws = theta, 
+                                 names = c('b', 
+                                           'mu', 
+                                           'phi_h', 
+                                           's2_h', 
+                                           'a'),
+                                 digits = 4,
+                                 hdp = TRUE
+  )
+  # Evaluation VaR
+  VaR_hat[ it ] = VaR(h_T = h_hmc[, dim( h_hmc )[2]],
+                      theta_hmc = theta
+  )
+  # Evaluation LPDS
+  lpds.star[ it ] = tvp_lpds_star(h_T = h_hmc[, dim( h_hmc )[2]],
+                                  theta_hmc = theta,
+                                  yobs = log.ret[T - horizon + it]
+  )
+  
+  if(it == horizon) time = Sys.time() - time
+  
+}
+
+time
+
+save(VaR_hat, lpds.star, summary, file = 'pcp_out_sample.RData')
