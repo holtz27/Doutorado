9825fc318be32b974e0abd90e1bb3f1bd393be43
holtz27 <97683235+holtz27@users.noreply.github.com>
2024-10-17 10:31:09 -0300

    Update waic.R

--- cobal/source/waic.R
+++ cobal/source/waic.R
@@ -1,4 +1,4 @@
-loglik = function(data_i, draws, data_, T){
+loglik = function(data_i, draws, data_, T, dyn = TRUE){
   # data_ = ( y_{1}, y_{2}, ..., y_{T} )
   # data_past = ( y_{0}, y_{1}, ..., y_{T-1} )
   # draws (2 * T + 3) x M
@@ -6,29 +6,45 @@ loglik = function(data_i, draws, data_, T){
   N = ncol( draws )
   log_l = rep(0, N)
   
-  for(col in 1:N){
-    
-    b = as.numeric( draws[1, col] )
-    h = as.numeric( draws[1 + k, col] )
-    a = as.numeric( draws[T + 1 + k, col] )
-    
-    log_l[ col ] = dnorm(as.numeric( data_i ), 
-                         mean = b + a * exp( h ), 
-                         sd = exp( 0.5 * h ), 
-                         log = TRUE)
+  if( dyn ){
+    for(col in 1:N){
+      
+      b = as.numeric( draws[1, col] )
+      h = as.numeric( draws[1 + k, col] )
+      a = as.numeric( draws[T + 1 + k, col] )
+      
+      log_l[ col ] = dnorm(as.numeric( data_i ), 
+                           mean = b + a * exp( h ), 
+                           sd = exp( 0.5 * h ), 
+                           log = TRUE)
+    }
+  }else{
+    for(col in 1:N){
+      
+      b = as.numeric( draws[1, col] )
+      h = as.numeric( draws[1 + k, col] )
+      a = as.numeric( draws[T + 2, col] )
+      
+      log_l[ col ] = dnorm(as.numeric( data_i ), 
+                           mean = b + a * exp( h ), 
+                           sd = exp( 0.5 * h ), 
+                           log = TRUE)
+    }
   }
+  
   
   return( log_l )
 }
 
 ############### waic
-waic = function(data, draws){
+waic = function(data, draws, dyn = TRUE){
   T = length( data )
   X = sapply(X = data, 
              FUN = loglik,
              draws,
              data,
-             length( data )
+             length( data ),
+             dyn
   ) 
   
   waic = loo::waic( X )
