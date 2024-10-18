87b4deace329f8b1650757ffa607d48c38f84270
holtz27 <97683235+holtz27@users.noreply.github.com>
2024-10-18 17:53:17 -0300

    Update simulacao1.R

--- Topicos/dynssv/st/simulacao1/simulacao1.R
+++ Topicos/dynssv/st/simulacao1/simulacao1.R
@@ -4,6 +4,7 @@ rtnorm = function(n){
   return( qnorm( 0.5 * (u + 1) ) )
 }
 source('https://raw.githubusercontent.com/holtz27/svmsmn/refs/heads/main/source/num_analisys.R')
+source('https://raw.githubusercontent.com/holtz27/Doutorado/refs/heads/main/cobal/source/forecast_vol.R')
 localenvir = rstudioapi::getActiveDocumentContext()
 path = localenvir$path
 dir = dirname(path)
@@ -14,143 +15,193 @@ model_stan2 = rstan::stan_model(file = path)
 path = paste0( dir, '/dynssv_st3.stan')
 model_stan3 = rstan::stan_model(file = path)
 
-set.seed( 8936381 ) # 164872
-T = 1e3
+#set.seed(164872)
+T = 1e2
 # log-volatility
 mu_h = 0
 phi_h = 0.99
-sigma_h = 0.15
+sigma_h = 0.1
 # dynskew
 mu_a = 0
 phi_a = 0.99
 sigma_a = 0.01
 v = 10
 
-theta_vdd = matrix(c(mu_h, phi_h, sigma_h, 
-                     mu_a, phi_a, log(sigma_a), 
-                     v), ncol = 1)
+theta_vdd = matrix(c(mu_h, phi_h, sigma_h,
+                     mu_a, phi_a, sigma_a,
+                     v), 
+                   ncol = 1)
 summary1 = summary2 = summary3 = list()
+Time = 0
 
-M = 100
-m = 5
+M = 10
+m = 1
 saver = M / m
 
-warmup = 2e3
-iters = 1e3
+k = 5
+err1 = err2 = err3 = matrix(nrow = k, ncol = M)
+
+warmup = 2.5e2
+iters = 1e2
 
 for(it in 1:M){
-  
   if( (M %% m) != 0 ) stop('M precisar ser divisivel por m!')
+  time = Sys.time()
   
-  if( it == 1 ) time = Sys.time()
+  cat( '\014' )
+  cat('Réplica: ', it, '/', M, '\n', '\n')
+  if( it == 1 ){
+    cat('Tempo restante total estimado: Calculando...', '\n' )
+  }else{
+    cat('Tempo restante total estimado: ', 
+        round((M - it + 1) * (Time / (it-1)), 1), 'mins',
+        '\n' )
+  }
+  
   #Data
-  a = delta = omega = rep(0 , T)
+  a = delta = omega = rep(0 , T + k)
   a[1] = mu_a + sigma_a / sqrt( 1 - phi_a * phi_a ) * rnorm(1)
-  for(t in 2:T) a[t] = mu_a + phi_a * (a[t-1] - mu_a) + sigma_a * rnorm(1)
+  for(t in 2:(T + k)) a[t] = mu_a + phi_a * (a[t-1] - mu_a) + sigma_a * rnorm(1)
   delta = a / sqrt(1 + a*a)
   k1 = sqrt(0.5 * v) * gamma(0.5*(v-1)) / gamma(0.5 * v)
   k2 = v / (v-2);
   omega = 1 / sqrt( k2 - 2 * ( delta * k1 )^2 / pi )
   mean_st = - sqrt(2 / pi) * delta * omega * k1
-  W = rtnorm(T)
-  U = rgamma(T, shape = 0.5 * v, rate = 0.5* v)
-  y = h = rep(0, T)
+  W = rtnorm(T + k)
+  U = rgamma(T + k, shape = 0.5 * v, rate = 0.5* v)
+  y = h = rep(0, T + k)
   h[ 1 ] = mu_h + sigma_h / sqrt( 1 - phi_h * phi_h ) * rnorm(1)
-  for(t in 2:T) h[ t ] = mu_h + phi_h * (h[ t-1 ] - mu_h) + sigma_h * rnorm(1)
+  for(t in 2:(T + k)) h[ t ] = mu_h + phi_h * (h[ t-1 ] - mu_h) + sigma_h * rnorm(1)
   mu_t = mean_st + omega * delta * W * exp(0.5 * h) / sqrt( U )
   sigma_t = omega * sqrt(1 - ( delta )^2) * exp(0.5 * h) / sqrt( U )
-  y = mu_t + sigma_t * rnorm(T)
+  y = mu_t + sigma_t * rnorm(T + k)
+  
+  y.train = y[1:T]
+  h.test = matrix( h[(T + 1):(T + k)], ncol = 1 )
   
   ### Sampling1
-  draws = rstan::sampling(model_stan1, 
-                          data = list(T = length( y ), 
-                                      y = as.numeric( y ),
-                                      lambda1 = 1.55,
-                                      lambda2 = 14.34
-                          ),
-                          chains = 1,
-                          warmup = warmup,
-                          iter = warmup + iters,
-                          cores = 1
-  )
-  x = rstan::extract(draws)
-  theta = matrix(x$mu_h, nrow = 1)
-  theta = rbind(theta, x$phi_h, x$s_h, x$mu_a, x$phi_a, x$ls_a, x$v)
-  summary1[[ it ]] = num_analisys(draws = theta, 
-                                  names = c('mu_h', 'phi_h', 's_h', 
-                                            'mu_a', 'phi_a', 'ls_a', 
-                                            'v'),
-                                  digits = 4,
-                                  hdp = TRUE
-                                  )
-
+  test = 1
+  while( test > 0 ){
+    draws = rstan::sampling(model_stan1, 
+                            data = list(T = length( y.train ), 
+                                        y = as.numeric( y.train ),
+                                        lambda1 = 1.55,
+                                        lambda2 = 14.34
+                            ),
+                            chains = 1,
+                            warmup = warmup,
+                            iter = warmup + iters,
+                            cores = 1
+    )
+    x = rstan::extract(draws)
+    theta = matrix(x$mu_h, nrow = 1)
+    theta = rbind(theta, x$phi_h, x$s_h, x$mu_a, x$phi_a, x$ls_a, x$v)
+    s = num_analisys(draws = theta, 
+                     names = c('mu_h', 'phi_h', 's_h', 
+                               'mu_a', 'phi_a', 'ls_a', 
+                               'v'),
+                     digits = 4,
+                     hdp = TRUE
+    )
+    test = sum( abs( s[ , 'CD'] ) > 1.96 ) 
+  }
+  summary1[[ it ]] = s
+  h_new = forecast.vol(h_T = apply(x$h, MARGIN = 1, tail, 1),
+                       mu = x$mu_h,
+                       phi = x$phi_h,
+                       sigma = x$s_h,
+                       k = k
+                      )$h_new
+  err1[, it] = exp( h_new ) - exp( h.test ) 
+  
   ### Sampling2
-  draws = rstan::sampling(model_stan2, 
-                          data = list(T = length( y ), 
-                                      y = as.numeric( y ),
-                                      lambda1 = 1.55,
-                                      lambda2 = 14.34
-                          ),
-                          chains = 1,
-                          warmup = warmup,
-                          iter = warmup + iters,
-                          cores = 1
-  )
-  x = rstan::extract(draws)
-  theta = matrix(x$mu_h, nrow = 1)
-  theta = rbind(theta, x$phi_h, x$s_h, x$mu_a, x$phi_a, x$ls_a, x$v)
-  summary2[[ it ]] = num_analisys(draws = theta, 
-                                  names = c('mu_h', 'phi_h', 's_h', 
-                                            'mu_a', 'phi_a', 'ls_a', 
-                                            'v'),
-                                  digits = 4,
-                                  hdp = TRUE
-  )
+  test = 1
+  while( test > 0 ){
+    draws = rstan::sampling(model_stan2, 
+                            data = list(T = length( y.train ), 
+                                        y = as.numeric( y.train ),
+                                        lambda1 = 1.55,
+                                        lambda2 = 14.34
+                            ),
+                            chains = 1,
+                            warmup = warmup,
+                            iter = warmup + iters,
+                            cores = 1
+    )
+    x = rstan::extract(draws)
+    theta = matrix(x$mu_h, nrow = 1)
+    theta = rbind(theta, x$phi_h, x$s_h, x$mu_a, x$phi_a, x$ls_a, x$v)
+    s = num_analisys(draws = theta, 
+                     names = c('mu_h', 'phi_h', 's_h', 
+                               'mu_a', 'phi_a', 'ls_a', 
+                               'v'),
+                     digits = 4,
+                     hdp = TRUE
+    )
+    test = sum( abs( s[ , 'CD'] ) > 1.96 )
+  }
+  summary2[[ it ]] = s
+  h_new = forecast.vol(h_T = apply(x$h, MARGIN = 1, tail, 1),
+                       mu = x$mu_h,
+                       phi = x$phi_h,
+                       sigma = x$s_h,
+                       k = k
+  )$h_new
+  err2[, it] = exp( h_new ) - exp( h.test )
+  
   
   ### Sampling3
-  draws = rstan::sampling(model_stan3, 
-                          data = list(T = length( y ), 
-                                      y = as.numeric( y ),
-                                      lambda1 = 1.55,
-                                      lambda2 = 14.34
-                          ),
-                          chains = 1,
-                          warmup = warmup,
-                          iter = warmup + iters,
-                          cores = 1
-  )
-  x = rstan::extract( draws )
-  theta = matrix(x$mu_h, nrow = 1)
-  theta = rbind(theta, x$phi_h, x$s_h, x$mu_a, x$phi_a, x$ls_a, x$v)
-  summary3[[ it ]] = num_analisys(draws = theta, 
-                                  names = c('mu_h', 'phi_h', 's_h', 
-                                            'mu_a', 'phi_a', 'ls_a', 
-                                            'v'),
-                                  digits = 4,
-                                  hdp = TRUE
-  )
+  test = 1
+  while( test > 0 ){
+    draws = rstan::sampling(model_stan3, 
+                            data = list(T = length( y.train ), 
+                                        y = as.numeric( y.train ),
+                                        lambda1 = 1.55,
+                                        lambda2 = 14.34
+                            ),
+                            chains = 1,
+                            warmup = warmup,
+                            iter = warmup + iters,
+                            cores = 1
+    )
+    x = rstan::extract( draws )
+    theta = matrix(x$mu_h, nrow = 1)
+    theta = rbind(theta, x$phi_h, x$s_h, x$mu_a, x$phi_a, x$ls_a, x$v)
+    s = num_analisys(draws = theta, 
+                     names = c('mu_h', 'phi_h', 's_h', 
+                               'mu_a', 'phi_a', 'ls_a', 
+                               'v'),
+                     digits = 4,
+                     hdp = TRUE
+    )
+    test = sum( abs( s[ , 'CD'] ) > 1.96 )
+  }
+  summary3[[ it ]] = s
+  h_new = forecast.vol(h_T = apply(x$h, MARGIN = 1, tail, 1),
+                       mu = x$mu_h,
+                       phi = x$phi_h,
+                       sigma = x$s_h,
+                       k = k
+  )$h_new
+  err3[, it] = exp( h_new ) - exp( h.test )
+  
   # saver
   if( it == saver ){
     
-    Summary = list( summary1 = summary1, 
-                    summary2 = summary2, 
-                    summary3 = summary3 )
-    
-    save( Summary, theta_vdd, file = paste0( dir, '/simulacao1.1.RData') )
+    save( summary1,
+          summary2,
+          summary3,
+          theta_vdd, 
+          err1,
+          err2,
+          err3,
+          file = 'simulacao_sa_0.01.RData'
+          )
     saver = saver + M / m
     
   }
-  if( it == M ) time = Sys.time() - time
-  
+  time = Sys.time() - time
+  Time = Time + as.numeric(time, units = 'mins')
 }
 
-time
-
-# Analazing...
-source('https://raw.githubusercontent.com/holtz27/Doutorado/refs/heads/main/source/res.sim.R')
-load('~/dynssv/st/simulacao1/simulacao1.1.RData')
-
-s = Summary$summary3
-y = res.sim( s, theta_vdd, med.abs = FALSE )
-y$errors
-y$metricas
+Time
