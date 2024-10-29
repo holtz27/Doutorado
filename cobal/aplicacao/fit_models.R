6ba58f4a2017cb002fddd98f02c87f200bed74d4
holtz27 <97683235+holtz27@users.noreply.github.com>
2024-10-29 16:52:59 -0300

    Update fit_models.R

--- cobal/aplicacao/fit_models.R
+++ cobal/aplicacao/fit_models.R
@@ -1,29 +1,33 @@
-source( 'https://raw.githubusercontent.com/holtz27/svmsmn/main/source/figures.R' )
+source('https://raw.githubusercontent.com/holtz27/svmsmn/main/source/figures.R')
 source('https://raw.githubusercontent.com/holtz27/svmsmn/refs/heads/main/source/num_analisys.R')
 source('https://raw.githubusercontent.com/holtz27/Doutorado/refs/heads/main/cobal/source/waic.R')
 source('https://raw.githubusercontent.com/holtz27/svmsmn/refs/heads/main/source/num_analisys.R')
 
-# Compilando o modelo Stan
-dir = paste0(getwd(), '/cobal/models/')
-path = paste0(dir, 'static.stan')
+# compiling stan models
+dir = paste0(getwd(), '/Doutorado/Eventos/cobal/')
+path = paste0(dir, 'models/static.stan')
 model_stan1 = rstan::stan_model(file = path)
-path = paste0(dir, 'ig.stan')
+path = paste0(dir, 'models/ig.stan')
 model_stan2 = rstan::stan_model(file = path)
-path = paste0(dir, 'pcp.stan')
+path = paste0(dir, 'models/pcp.stan')
 model_stan3 = rstan::stan_model(file = path)
-path = paste0(dir, 'jeffrey.stan')
+path = paste0(dir, 'models/jeffrey.stan')
 model_stan4 = rstan::stan_model(file = path)
 
-summary = list('static', 'ig', 'pcp',  'jeffrey')
+# images directory
+dir_images = paste0( dir, 'aplication/images/cobre/' )
+
+summary = list('static', 'ig', 'pcp', 'jeffrey')
 waic.criterion = matrix(0, nrow = 1, ncol = 4)
-row.names( waic.criterion ) = 'waic'
 colnames( waic.criterion ) = c('static', 'ig', 'pcp', 'jeffrey')
 
-warmup = 3e3
-iter = 2e3
+warmup = 1e2
+iter = 1e1
 
 for( i in 1:1 ){
-  # fitting model1
+  ##################
+  # fitting model1 #
+  ##################
   draws = rstan::sampling(model_stan1, 
                           data = list(T = length( log.ret ), 
                                       y = as.numeric( log.ret )
@@ -37,26 +41,31 @@ for( i in 1:1 ){
   theta = matrix(x$b, nrow = 1)
   theta = rbind(theta, x$mu, x$phi, x$s_h, x$a)
   summary$static = num_analisys(draws = theta, 
-                                names = c('b', 
-                                          'mu', 
-                                          'phi_h', 
-                                          's_h', 
-                                          'a'),
-                                digits = 4,
-                                hdp = FALSE
+                                  names = c('b', 
+                                            'mu', 
+                                            'phi_h', 
+                                            's_h', 
+                                            'a'),
+                                  digits = 4,
+                                  hdp = FALSE
   )
   # Plots
-  pdf( 'static.pdf', width = 20, height = 10 )
+  pdf( paste0( dir_images, 'static.pdf'), 
+       width = 20, height = 10 )
   trace_plots(theta,
               burn = 0, lags = 1,
-              names = c('b', 'mu', 'phi', 's_h', 'a') )
+              names = c('b', 'mu', 'phi', 's2_h', 'a') )
   dev.off()
+  h_hat1 = apply( x$h, MARGIN = 2, mean )
+  # waic
   WAIC = waic(data = log.ret, 
               draws = rbind(x$b, t(x$h), x$a ),
               dyn = FALSE)
   waic.criterion[1] = WAIC$estimates['waic', 1]
   
-  # fitting model2
+  ##################
+  # fitting model2 #
+  ##################
   draws = rstan::sampling(model_stan2, 
                           data = list(T = length( log.ret ), 
                                       y = as.numeric( log.ret )
@@ -68,35 +77,53 @@ for( i in 1:1 ){
   )
   x = rstan::extract(draws)
   theta = matrix(x$b, nrow = 1)
-  theta = rbind(theta, x$mu, x$phi, x$s_h, x$ls_a)
+  theta = rbind(theta, x$mu, x$phi, x$s_h, x$ls_a, x$a1)
   # Numeric Analysis
   summary$ig = num_analisys(draws = theta, 
                             names = c('b', 
                                       'mu', 
                                       'phi_h', 
                                       's_h', 
-                                      'ls_a'),
+                                      'ls_a',
+                                      'a1'),
                             digits = 4,
                             hdp = FALSE
   )
   # Plots
-  pdf( 'ig.pdf', width = 20, height = 10 )
+  pdf( paste0( dir_images, 'ig.pdf'), 
+       width = 20, height = 10 )
   trace_plots(theta,
               burn = 0, lags = 1,
-              names = c('b', 'mu', 'phi', 's_h', 'ls_a')
-  )
+              names = c('b', 'mu', 'phi', 's2_h', 'ls_a', 'a1')
+              )
   dev.off()
+  
+  pdf( paste0( dir_images, 'ig_a.pdf'), 
+       width = 20, height = 10 )
+  a_hat = apply( x$a, MARGIN = 2, mean )
+  a_min = apply( x$a, MARGIN = 2, quantile, probs = 0.025 )
+  a_max = apply( x$a, MARGIN = 2, quantile, probs = 0.975 )
+  plot( a_hat, type = 'l' , ylim = c(min(a_min), max(a_max)) )
+  lines( a_min, type = 'l', lty = 2 )
+  lines( a_max, type = 'l', lty = 2 )
+  abline( h = 0, col = 'grey', lty = 2)
+  dev.off()
+  
+  h_hat2 = apply( x$h, MARGIN = 2, mean )
+  # waic
   WAIC = waic(data = log.ret, 
               draws = rbind(x$b, t(x$h), t(x$a)),
               dyn = FALSE)
   waic.criterion[ 2 ] = WAIC$estimates['waic', 1]
   
-  # fitting model3
+  ##################
+  # fitting model3 #
+  ##################
   draws = rstan::sampling(model_stan3, 
                           data = list(T = length( log.ret ), 
                                       y = as.numeric( log.ret ),
                                       lambda = -log(0.5) / sqrt(0.5)
-                          ),
+                                      ),
                           chains = 1,
                           warmup = warmup,
                           iter = warmup + iter,
@@ -104,33 +131,50 @@ for( i in 1:1 ){
   )
   x = rstan::extract(draws)
   theta = matrix(x$b, nrow = 1)
-  theta = rbind(theta, x$mu, x$phi, x$s_h, x$ls_a)
+  theta = rbind(theta, x$mu, x$phi, x$s_h, x$ls_a, x$a1)
   # Numeric Analysis
   summary$pcp = num_analisys(draws = theta, 
                              names = c('b', 
                                        'mu', 
                                        'phi_h', 
                                        's_h', 
-                                       'ls_a'),
+                                       'ls_a',
+                                       'a1'),
                              digits = 4,
                              hdp = FALSE
   )
   # Plots
-  pdf( 'pcp.pdf', width = 20, height = 10 )
+  pdf( paste0( dir_images, 'pcp.pdf'), 
+       width = 20, height = 10 )
   trace_plots(theta,
               burn = 0, lags = 1,
-              names = c('b', 'mu', 'phi', 's_h', 'ls_a') )
+              names = c('b', 'mu', 'phi', 's2_h', 'ls_a', 'a1') )
   dev.off()
+  
+  pdf( paste0( dir_images, 'pcp_a.pdf'), 
+       width = 20, height = 10 )
+  a_hat = apply( x$a, MARGIN = 2, mean )
+  a_min = apply( x$a, MARGIN = 2, quantile, probs = 0.025 )
+  a_max = apply( x$a, MARGIN = 2, quantile, probs = 0.975 )
+  plot( a_hat, type = 'l' , ylim = c(min(a_min), max(a_max)) )
+  lines( a_min, type = 'l', lty = 2 )
+  lines( a_max, type = 'l', lty = 2 )
+  abline( h = 0, col = 'grey', lty = 2)
+  dev.off()
+  h_hat3 = apply( x$h, MARGIN = 2, mean )
+  # waic
   WAIC = waic(data = log.ret, 
               draws = rbind(x$b, t(x$h), t(x$a)),
               dyn = FALSE)
   waic.criterion[ 3 ] = WAIC$estimates['waic', 1]
   
-  # fitting model4
+  ##################
+  # fitting model4 #
+  ##################
   draws = rstan::sampling(model_stan4, 
                           data = list(T = length( log.ret ), 
                                       y = as.numeric( log.ret )
-                          ),
+                                      ),
                           chains = 1,
                           warmup = warmup,
                           iter = warmup + iter,
@@ -138,23 +182,39 @@ for( i in 1:1 ){
   )
   x = rstan::extract(draws)
   theta = matrix(x$b, nrow = 1)
-  theta = rbind(theta, x$mu, x$phi, x$s_h, x$ls_a)
+  theta = rbind(theta, x$mu, x$phi, x$s_h, x$ls_a, x$a1)
   # Numeric Analysis
-  summary$exp = num_analisys(draws = theta, 
-                             names = c('b', 
-                                       'mu', 
-                                       'phi_h', 
-                                       's_h', 
-                                       'ls_a'),
-                             digits = 4,
-                             hdp = FALSE
+  summary$jeffrey = num_analisys(draws = theta, 
+                                 names = c('b', 
+                                           'mu', 
+                                           'phi_h', 
+                                           's_h', 
+                                           'ls_a',
+                                           'a1'),
+                                 digits = 4,
+                                 hdp = FALSE
   )
   # Plots
-  pdf( 'jeffrey.pdf', width = 20, height = 10 )
+  pdf( paste0( dir_images, 'jeffrey.pdf'), 
+       width = 20, height = 10 )
   trace_plots(theta,
               burn = 0, lags = 1,
-              names = c('b', 'mu', 'phi', 's_h', 'ls_a') )
+              names = c('b', 'mu', 'phi', 's2_h', 'ls_a', 'a1') )
   dev.off()
+  
+  pdf( paste0( dir_images, 'jeffrey_a.pdf'), 
+       width = 20, height = 10 )
+  a_hat = apply( x$a, MARGIN = 2, mean )
+  a_min = apply( x$a, MARGIN = 2, quantile, probs = 0.025 )
+  a_max = apply( x$a, MARGIN = 2, quantile, probs = 0.975 )
+  plot( a_hat, type = 'l' , ylim = c(min(a_min), max(a_max)) )
+  lines( a_min, type = 'l', lty = 2 )
+  lines( a_max, type = 'l', lty = 2 )
+  abline( h = 0, col = 'grey', lty = 2)
+  dev.off()
+  
+  h_hat4 = apply( x$h, MARGIN = 2, mean )
+  # waic
   WAIC = waic(data = log.ret, 
               draws = rbind(x$b, t(x$h), t(x$a)),
               dyn = FALSE)
@@ -167,4 +227,18 @@ waic.criterion = rbind(waic.criterion,
                        as.numeric(waic.criterion[1, ] == min( waic.criterion)))
 waic.criterion
 
-
+# Volatilities
+plot(dates[-1], abs(log.ret), 
+     col = 'gray', 
+     xlab = '', ylab = '|Return|',
+     type = 'l', cex.axis = 1.5, cex.lab = 1.5)
+lines(dates[-1], exp(0.5 * h_hat1), lwd = 2, lty = 1 )
+lines(dates[-1], exp(0.5 * h_hat2), lwd = 2, lty = 2, col = 'green' )
+lines(dates[-1], exp(0.5 * h_hat3), lwd = 2, lty = 1, col = 'red' )
+lines(dates[-1], exp(0.5 * h_hat4), lwd = 2, lty = 2, col = 'blue' )
+legend('topright', 
+       legend = c('Static', 'IG', 'PCP', 'Jeffrey'), 
+       col = c('black', 'green', 'red', 'blue'), 
+       lty = c(1, 2, 1, 2), 
+       lwd = 2, 
+       bty = 'n')
