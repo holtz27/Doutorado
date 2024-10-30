1eb6719b6b6385eef2d47e2aa00b8e6d6facaafe
holtz27 <97683235+holtz27@users.noreply.github.com>
2024-10-30 11:08:18 -0300

    Add files via upload

--- cobal/aplicacao/predicao/pred.R
+++ cobal/aplicacao/predicao/pred.R
@@ -15,11 +15,16 @@ path = paste0(dir, 'models/pcp.stan')
 model_stan3 = rstan::stan_model(file = path)
 path = paste0(dir, 'models/jeffrey.stan')
 model_stan4 = rstan::stan_model(file = path)
+path = paste0(dir, 'models/exp.stan')
+model_stan5 = rstan::stan_model(file = path)
 
-horizon = 10
-VaR_hat1 = VaR_hat2 = VaR_hat3 = VaR_hat4 = rep(0, horizon)
-lpds.star1 = lpds.star2 = lpds.star3 = lpds.star4 = rep(0, horizon)
-summary1 = summary2 = summary3 = summary4 = list()
+# outs directory
+dir_out = paste0( dir, 'aplication/data/cobre/' )
+
+horizon = 1
+VaR_hat1 = VaR_hat2 = VaR_hat3 = VaR_hat4 = VaR_hat5 = rep(0, horizon)
+lpds.star1 = lpds.star2 = lpds.star3 = lpds.star4 = lpds.star5 = rep(0, horizon)
+summary1 = summary2 = summary3 = summary4 = summary5 = list()
 Time = 0
 
 warmup = 2e1
@@ -222,21 +227,73 @@ for(it in 1:horizon){
                                    theta_hmc = theta,
                                    yobs = log.ret[T - horizon + it]
   )
+  ##########
+  # Model5 #
+  ##########
+  cat( '\014' )
+  cat('Horizon: ', it, '/', horizon, '\n', '\n')
+  if( it == 1 ){
+    cat('Estimated total remaining time: Calculating...', '\n', '\n' )
+  }else{
+    cat('Estimated total remaining time: ', 
+        round((horizon - it + 1) * (Time / (it-1)), 1), 'hs',
+        '\n', '\n' )
+  }
+  cat( 'Model 5', '\n' )
+  draws = rstan::sampling(model_stan5, 
+                          data = list(T = length( y.now ), 
+                                      y = as.numeric( y.now )
+                          ),
+                          chains = 1,
+                          warmup = warmup,
+                          iter = warmup + iters,
+                          cores = 1
+  )
+  x = rstan::extract(draws)
+  theta = matrix(x$b, nrow = 1)
+  theta = rbind(theta, x$mu, x$phi, x$s2_h, x$s2_a)
+  h_hmc = x$h
+  a_hmc = x$a
+  # Summary
+  summary5[[ it ]] = num_analisys(draws = theta, 
+                                  names = c('b', 
+                                            'mu', 
+                                            'phi_h', 
+                                            's2_h', 
+                                            'ls_a'),
+                                  digits = 4,
+                                  hdp = TRUE
+  )
+  # Evaluation VaR
+  VaR_hat5[ it ] = tvp_VaR(h_T = h_hmc[, dim( h_hmc )[2]],
+                           a_T = a_hmc[, dim( h_hmc )[2]],
+                           theta_hmc = theta
+  )
+  # Evaluation LPDS
+  lpds.star5[ it ] = tvp_lpds_star(h_T = h_hmc[, dim( h_hmc )[2]],
+                                   a_T = a_hmc[, dim( h_hmc )[2]],
+                                   theta_hmc = theta,
+                                   yobs = log.ret[T - horizon + it]
+  )
+  
   time = Sys.time() - time
   Time = Time + as.numeric(time, units = 'hours')
   if(it == horizon){
     save(VaR_hat1, lpds.star1, summary1, 
-         file = paste0( dir, 'aplication/prediction/static.RData' )
+         file = paste0( dir_out, 'static.RData' )
          )
     save(VaR_hat2, lpds.star2, summary2, 
-         file = paste0( dir, 'aplication/prediction/ig.RData' )
+         file = paste0( dir_out, 'ig.RData' )
          )
     save(VaR_hat3, lpds.star3, summary3, 
-         file = paste0( dir, 'aplication/prediction/pcp.RData' )
+         file = paste0( dir_out, 'pcp.RData' )
          )
     save(VaR_hat4, lpds.star4, summary4, 
-         file = paste0( dir, 'aplication/prediction/jeffrey.RData' )
+         file = paste0( dir_out, 'jeffrey.RData' )
          )
+    save(VaR_hat5, lpds.star5, summary5, 
+         file = paste0( dir_out, 'exp.RData' )
+    )
   } 
   
 }
