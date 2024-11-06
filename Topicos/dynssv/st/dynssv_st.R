048b4c8fab32c18671a3acbe4f0cf8ec2d384514
holtz27 <97683235+holtz27@users.noreply.github.com>
2024-11-06 16:26:17 -0300

    Add files via upload

--- Topicos/dynssv/st/dynssv_st.R
+++ Topicos/dynssv/st/dynssv_st.R
@@ -13,19 +13,19 @@ dir = dirname(path)
 path = paste0( dir, '/dynssv_st.stan')
 model_stan = rstan::stan_model(file = path)
 
-set.seed(164872)
-T = 1.5e3
+set.seed(1648723)
+T = 3e3
 # log-volatility
 mu_h = 0
 phi_h = 0.99
-sigma_h = 0.1
+s_h = 0.1
 # dynskew
-mu_a = -0.5
 phi_a = 0.99
-sigma_a = 0.1
+s_a = 0.0 # { 0, 0.05, 0.1 }
+
 a = delta = omega = rep(0 , T)
-a[1] = mu_a + sigma_a / sqrt( 1 - phi_a * phi_a ) * rnorm(1)
-for(t in 2:T) a[t] = mu_a + phi_a * (a[t-1] - mu_a) + sigma_a * rnorm(1)
+a[1] = 0
+for(t in 2:T) a[t] = phi_a * a[t-1] + s_a * rnorm(1)
 delta = a / sqrt(1 + a*a)
 
 v = 10
@@ -37,15 +37,16 @@ mean_st = - sqrt(2 / pi) * delta * omega * k1
 W = rtnorm(T)
 U = rgamma(T, shape = 0.5 * v, rate = 0.5* v)
 y = h = rep(0, T)
-h[ 1 ] = mu_h + sigma_h / sqrt( 1 - phi_h * phi_h ) * rnorm(1)
-for(t in 2:T) h[ t ] = mu_h + phi_h * (h[ t-1 ] - mu_h) + sigma_h * rnorm(1)
+h[ 1 ] = mu_h + s_h / sqrt( 1 - phi_h * phi_h ) * rnorm(1)
+for(t in 2:T) h[ t ] = mu_h + phi_h * (h[ t-1 ] - mu_h) + s_h * rnorm(1)
 mu_t = mean_st + omega * delta * W * exp(0.5 * h) / sqrt( U )
 sigma_t = omega * sqrt(1 - ( delta )^2) * exp(0.5 * h) / sqrt( U )
 y = mu_t + sigma_t * rnorm(T)
-par( mfrow = c(1, 2))
+par( mfrow = c(2, 2))
 plot( y , type = 'l' )
 hist( y, breaks = 40, probability = TRUE )
 abline(v = mean( y ), col = 'red', lty = 2)
+plot( a, type = 'l', col = 'red' )
 par( mfrow = c(1, 1) )
 data_summary = matrix(c( mean( y ),
                          sd( y ),
@@ -55,33 +56,36 @@ data_summary = matrix(c( mean( y ),
                          moments::kurtosis( y ) ), nrow = 1)
 colnames( data_summary ) = c('mean', 'sd', 'min', 'max', 'skewness', 'kurtosis')
 round( data_summary, digits = 4 )
+
 ################
 ### Sampling ###
 ################
 draws = rstan::sampling(model_stan, 
                         data = list(T = length( y ), 
                                     y = as.numeric( y ),
-                                    lambda1 = 1.55,
-                                    lambda2 = 14.34
+                                    lambda1 = 9.473,
+                                    lambda2 = -log( 0.5 ) / sqrt( 0.5 )
                         ),
                         chains = 1,
-                        warmup = 5e3,
-                        iter = 5e3 + 1e3,
+                        warmup = 3e3,
+                        iter = 3e3 + 2e3,
                         cores = 1
 )
-x = rstan::extract(draws)
-theta = matrix(x$mu_h, nrow = 1)
-theta = rbind(theta, x$phi_h, x$s_h, x$mu_a, x$phi_a, x$s_a, x$v)
+x = rstan::extract(draws, 
+                   pars = c('mu', 'phi_h', 's_h', 'phi_a', 'ls_a', 'a1', 'h', 'a', 'v')
+                   )
+theta = matrix(x$mu, nrow = 1)
+theta = rbind(theta, x$phi_h, x$s_h, x$phi_a, x$ls_a, x$a1, x$v)
 # Plots
 #pdf( 'pcp_traces.pdf', width = 20, height = 10 )
 trace_plots(theta,
             burn = 0, lags = 1,
-            names = c(expression(mu[h]), 
+            names = c(expression(mu), 
                       expression(phi[h]),
                       expression(sigma[h]),
-                      expression(mu[a]),
                       expression(phi[a]),
-                      expression(log(sigma[a]) ),
+                      expression(log(sigma[a])),
+                      expression(a[1]),
                       expression(nu)
                       ) 
             )
@@ -90,13 +94,14 @@ trace_plots(theta,
 # Numeric Analysis
 num_analisys(draws = theta, 
              names = c('mu_h', 'phi_h', 's_h', 
-                       'mu_a', 'phi_a', 'ls_a', 
+                       'phi_a', 'ls_a', 'a1',
                        'v'),
              digits = 4,
              hdp = TRUE
 )
 
 # h
+#pdf( 'pcp_h.pdf', width = 20, height = 10 )
 draws_h = x$h
 h_hat = apply( draws_h, MARGIN = 2, mean )
 h_min = apply( draws_h, MARGIN = 2, quantile, probs = 0.025 )
@@ -105,13 +110,21 @@ plot( h, type = 'l', col = 'red', ylim = c(min(h_min), max(h_max)) )
 lines( h_hat, type = 'l')
 lines( h_min, type = 'l', lty = 2 )
 lines( h_max, type = 'l', lty = 2 )
+#dev.off()
 
 # a
+#pdf( '2ig_a.pdf', width = 20, height = 10 )
 draws_a = x$a
 a_hat = apply( draws_a, MARGIN = 2, mean )
 a_min = apply( draws_a, MARGIN = 2, quantile, probs = 0.025 )
 a_max = apply( draws_a, MARGIN = 2, quantile, probs = 0.975 )
-plot( a_hat, type = 'l' , ylim = c(min(a_min), max(a_max)) )
+plot( a_hat, type = 'l', ylim = c(min(a_min), max(a_max)) )
 lines( a_min, type = 'l', lty = 2 )
 lines( a_max, type = 'l', lty = 2 )
 lines( a, type = 'l', col = 'red')
+#dev.off()
+
+
+
+
+
--- Topicos/dynssv/st/dynssv_st.stan
+++ Topicos/dynssv/st/dynssv_st.stan
@@ -5,8 +5,7 @@ data {
   real<lower=0> lambda2;
 }
 parameters{
-  real mu_h;
-  real mu_a;
+  real mu;
   real<lower=0, upper=1> phiT_h;
   real<lower=0, upper=1> phiT_a;
   real<lower=0> s_h;
@@ -15,10 +14,9 @@ parameters{
   vector[T] a_std;
   vector<lower=0>[T] W;
   vector<lower=0>[T] U;
-  
+  real a1;
   real<lower=2> v;
-  real<lower=0> km;
-  real<lower=0> ks;
+  real<lower=0> k;
 }
 transformed parameters{
   real<lower=-1,upper=1> phi_h;
@@ -26,7 +24,7 @@ transformed parameters{
   real<lower=0> s2_h;
   real<lower=0> s2_a;
   vector[T] a = a_std * s_a;
-  vector[T] h = h_std * s_h;  
+  vector[T] h = h_std * s_h;
   real ls_a = log( s_a );
   phi_h = ( 2 * phiT_h - 1 );
   phi_a = ( 2 * phiT_a - 1 );
@@ -34,14 +32,13 @@ transformed parameters{
   s2_a = pow( s_a, 2 );
   
   h[1] /= sqrt(1 - phi_h * phi_h);
-  a[1] /= sqrt(1 - phi_a * phi_a);
-  h += mu_h;
-  a += mu_a;
+  h += mu;
+  a[1] = a1;
   for (t in 2:T){
-    h[t] += phi_h * (h[t - 1] - mu_h);
-    a[t] += phi_a * (a[t - 1] - mu_a);
+    h[t] += phi_h * (h[t - 1] - mu);
+    a[t] += phi_a * a[t - 1];
   }
-  
+
   vector<lower=-1, upper=1>[T] delta = a ./ sqrt(1 + square(a) );
   real k1 = sqrt(0.5 * v) * tgamma(0.5*(v-1)) / tgamma(0.5 * v);
   real k2 = v / (v-2);
@@ -58,19 +55,17 @@ model {
   s2_h ~ inv_gamma( 2.5, 0.025 );
   
   // Prioris a
-  km ~ gamma(0.1, 0.1);
-  mu_a ~ double_exponential( 0, km );
-  //mu_a ~ normal( 0, sqrt(10) );
+  a1 ~ normal(0, sqrt(10));
   
   //priori1
-  //s2_a ~ weibull( 0.5, pow(lambda2, -2 ) );
+  s2_a ~ weibull( 0.5, pow(lambda2, -2 ) );
   
   //prior2
-  s2_a ~ inv_gamma( 2.5, 0.025 );
+  //s2_a ~ inv_gamma( 2.5, 0.025 );
   
   //prior3
-  //ks ~ gamma(0.1, 0.1);
-  //s_a ~ exponential( ks );
+  //k2 ~ gamma( 0.1, 0.1 );
+  //s_a ~ exponential( 1 / k2 );
   
   target += - lambda1 * sqrt(1 - phi_a) - 0.5 * log(1 - phi_a);
   
