acf8f841ccefc839127effd6d3f16507fdf4f37b
holtz27 <97683235+holtz27@users.noreply.github.com>
2024-11-14 20:14:30 -0300

    Create waic.R

---
+++ Topicos/waic.R
@@ -0,0 +1,12 @@
+############### waic
+waic = function(data, mut, st){
+  n = length(data)
+  M = matrix(nrow = dim(mut)[1], ncol = n)
+  for(j in 1:n){
+    M[, j] = dnorm(data[j], 
+                   mean = mut[, j], 
+                   sd = st[, j], 
+                   log = TRUE)
+  }
+  return(loo::waic(M))
+}
