0ec03cacd97318ac56e04d8a48bd2938b4df5946
holtz27 <97683235+holtz27@users.noreply.github.com>
2024-11-15 18:39:12 -0300

    Update fit_models.R

--- Topicos/dynssv/st/aplicacao/fit_models.R
+++ Topicos/dynssv/st/aplicacao/fit_models.R
@@ -4,6 +4,8 @@ source('https://raw.githubusercontent.com/holtz27/Doutorado/refs/heads/main/Topi
 
 # compiling stan models
 model.dir = '~/Doutorado/Pesquisa/Papers/Topicos/DS-SV/Code/st/models'
+path = paste0( model.dir, '/static.stan')
+model_stan0 = rstan::stan_model(file = path)
 path = paste0( model.dir, '/ig.stan')
 model_stan1 = rstan::stan_model(file = path)
 path = paste0(model.dir, '/pcp.stan')
@@ -13,14 +15,58 @@ model_stan3 = rstan::stan_model(file = path)
 
 out.dir = '~/Doutorado/Pesquisa/Papers/Topicos/DS-SV/Code/st/aplicacao'
 
-summary = list('ig', 'pcp', 'exp')
-waic.criterion = matrix(0, nrow = 1, ncol = 3)
-colnames(waic.criterion) = c('ig', 'pcp', 'exp')
+summary = list('static','ig', 'pcp', 'exp')
+waic.criterion = matrix(0, nrow = 1, ncol = 4)
+colnames(waic.criterion) = c('static', 'ig', 'pcp', 'exp')
 
 warmup = 3e3
 iter = 2e3
 
 for( i in 1:1 ){
+  
+  ##################
+  # fitting model0 #
+  ##################
+  test = 1
+  while( test > 0 || is.na(test) || is.nan(test) ){
+    draws = rstan::sampling(model_stan0, 
+                            data = list(T = length( log.ret ), 
+                                        y = as.numeric( log.ret )
+                            ),
+                            chains = 1,
+                            warmup = warmup,
+                            iter = warmup + iter,
+                            cores = 1
+    )
+    x = rstan::extract(draws, 
+                       pars = c('mu', 'phi_h', 's_h', 'h', 'a', 'v', 'mu_t', 'sigma_t')
+    )
+    theta = matrix(x$mu, nrow = 1)
+    theta = rbind(theta, x$phi_h, x$s_h, x$a, x$v)
+    summary$static = num_analisys(draws = theta, 
+                                  names = c('mu', 'phi_h', 's_h', 'a', 'v'),
+                                  digits = 4,
+                                  hdp = TRUE
+    )
+    test = sum( abs( summary$static[ , 'CD'] ) > 1.96 )
+  }
+  # Plots
+  pdf( paste0( out.dir, '/static.pdf'), width = 20, height = 10 )
+  trace_plots(theta,
+              burn = 0, lags = 1,
+              names = c(expression(mu), 
+                        expression(phi[h]),
+                        expression(sigma[h]),
+                        expression(alpha),
+                        expression(nu)
+              ) 
+  )
+  dev.off()
+  h_hat1 = apply( x$h, MARGIN = 2, mean )
+  # waic
+  WAIC = waic(data = log.ret, mut = x$mu_t, st = x$sigma_t)$estimates['waic', 1]
+  waic.criterion[ 1 ] = WAIC
+  
   ##################
   # fitting model1 #
   ##################
@@ -53,7 +99,7 @@ for( i in 1:1 ){
     test = sum( abs( summary$ig[ , 'CD'] ) > 1.96 )
   }
   # Plots
-  pdf( paste0( out.dir, 'ig.pdf'), width = 20, height = 10 )
+  pdf( paste0( out.dir, '/ig.pdf'), width = 20, height = 10 )
   trace_plots(theta,
               burn = 0, lags = 1,
               names = c(expression(mu), 
@@ -65,10 +111,10 @@ for( i in 1:1 ){
               ) 
   )
   dev.off()
-  h_hat1 = apply( x$h, MARGIN = 2, mean )
+  h_hat2 = apply( x$h, MARGIN = 2, mean )
   # waic
   WAIC = waic(data = log.ret, mut = x$mu_t, st = x$sigma_t)$estimates['waic', 1]
-  waic.criterion[1] = WAIC
+  waic.criterion[ 2 ] = WAIC
   
   ##################
   # fitting model2 #
@@ -101,10 +147,10 @@ for( i in 1:1 ){
                                digits = 4,
                                hdp = TRUE
     )
-    test = sum( abs( summary$ig[ , 'CD'] ) > 1.96 )
+    test = sum( abs( summary$pcp[ , 'CD'] ) > 1.96 )
   }
   # Plots
-  pdf( paste0( out.dir, 'pcp.pdf'), width = 20, height = 10 )
+  pdf( paste0( out.dir, '/pcp.pdf'), width = 20, height = 10 )
   trace_plots(theta,
               burn = 0, lags = 1,
               names = c(expression(mu), 
@@ -128,10 +174,10 @@ for( i in 1:1 ){
   #abline( h = 0, col = 'grey', lty = 2)
   #dev.off()
   
-  h_hat2 = apply( x$h, MARGIN = 2, mean )
+  h_hat3 = apply( x$h, MARGIN = 2, mean )
   # waic
   WAIC = waic(data = y, mut = x$mu_t, st = x$sigma_t)$estimates['waic', 1]
-  waic.criterion[ 2 ] = WAIC
+  waic.criterion[ 3 ] = WAIC
   
   ##################
   # fitting model3 #
@@ -164,11 +210,11 @@ for( i in 1:1 ){
                                 digits = 4,
                                 hdp = TRUE
     )
-    test = sum( abs( summary$pcp[ , 'CD'] ) > 1.96 )
+    test = sum( abs( summary$exp[ , 'CD'] ) > 1.96 )
   }
   
   # Plots
-  pdf( paste0( out.dir, 'exp.pdf'), width = 20, height = 10 )
+  pdf( paste0( out.dir, '/exp.pdf'), width = 20, height = 10 )
   trace_plots(theta,
               burn = 0, lags = 1,
               names = c(expression(mu), 
@@ -191,16 +237,16 @@ for( i in 1:1 ){
   #lines( a_max, type = 'l', lty = 2 )
   #abline( h = 0, col = 'grey', lty = 2)
   #dev.off()
-  h_hat3 = apply( x$h, MARGIN = 2, mean )
+  h_hat4 = apply( x$h, MARGIN = 2, mean )
   # waic
   WAIC = waic(data = y, mut = x$mu_t, st = x$sigma_t)$estimates['waic', 1]
-  waic.criterion[ 3 ] = WAIC
+  waic.criterion[ 4 ] = WAIC
   
   # Save
   save(
     summary, 
     waic.criterion,
-    h_hat1, h_hat2, h_hat3,
+    h_hat1, h_hat2, h_hat3, h_hat4,
     file = paste0(dir_out, 'nasdaq.RData')
   )  
 }
@@ -210,26 +256,6 @@ waic.criterion = rbind(waic.criterion,
                        as.numeric(waic.criterion[1, ] == min( waic.criterion)))
 waic.criterion
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 # Figure Volatilities
 plot(dates[-1], abs(log.ret), 
      col = 'gray', 
@@ -238,11 +264,10 @@ plot(dates[-1], abs(log.ret),
 lines(dates[-1], exp(0.5 * h_hat1), lwd = 2, lty = 1 )
 lines(dates[-1], exp(0.5 * h_hat2), lwd = 2, lty = 2, col = 'green' )
 lines(dates[-1], exp(0.5 * h_hat3), lwd = 2, lty = 1, col = 'red' )
-lines(dates[-1], exp(0.5 * h_hat5), lwd = 2, lty = 2, col = 'blue' )
 lines(dates[-1], exp(0.5 * h_hat4), lwd = 2, lty = 2, col = 'orange' )
 legend('topright', 
-       legend = c('Static', 'IG', 'PCP', 'Jeffrey', 'Exp'), 
-       col = c('black', 'green', 'red', 'blue', 'orange'), 
-       lty = c(1, 2, 1, 2, 1), 
+       legend = c('Static', 'IG', 'PCP', 'Exp'), 
+       col = c('black', 'green', 'red', 'orange'), 
+       lty = c(1, 2, 1, 2), 
        lwd = 2, 
        bty = 'n')
