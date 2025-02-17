2187e2e26fd0dc6ae4e6ffd219143bd82290a8f3
holtz27 <97683235+holtz27@users.noreply.github.com>
2025-02-16 22:42:57 -0300

    Add files via upload

--- HMMcodes/mlogLk_Rcpp.cpp
+++ HMMcodes/mlogLk_Rcpp.cpp
@@ -1,24 +1,40 @@
-#include <RcppArmadillo.h>
 // [[Rcpp::depends(RcppArmadillo)]]
+
+#include <RcppArmadillo.h>
+#include<cmath>
+
 using namespace std;
 using namespace Rcpp;
-#include<iostream>
-#include<cmath>
-#include <time.h>
+using namespace arma;
 
 // [[Rcpp::export]]
-double mlogLk_Rcpp(arma::mat allprobs, arma::mat egamma, arma::rowvec foo, int n)
-{
-  double lscale=0;
-  int i=0;
+double mlogLk_Rcpp(mat allprobs, mat egamma, rowvec foo, int n){
+  
+  if(allprobs.has_nan()) stop("Probs must be a number");
+  if(allprobs.has_inf()) stop("Probs must be a finite number");
+  
+  if(egamma.has_nan()) stop("Transition Probs must be a number");
+  if(egamma.has_inf()) stop("Transition Probs must be a finite number");
+  
+  if(foo.has_nan()) stop("Initial Probs must be a number");
+  if(foo.has_inf()) stop("Initial Probs must be a finite number");
+  
+  double lscale=1;
 	double sumfoo;
-
-	for(i=1;i<n;i++)
-	{		
-		foo=foo*egamma%allprobs.row(i);
+  rowvec pivot;
+  
+	for(int i=1; i<n; i++){
+	  pivot = foo*egamma; 
+		foo = pivot%allprobs.row(i);
+		
 		sumfoo=sum(foo);
+		if(sumfoo!=0){
+		  foo=foo/sumfoo;  
+		}else{
+		  stop("sumfoo must be positive!");
+		}
+		
 		lscale=lscale+log(sumfoo);
-		foo=foo/sumfoo;
 	}
 
 	return lscale;
