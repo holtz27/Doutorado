4b54f37bdd76c196b8e4180ab7853e8a0e78e8e9
holtz27 <97683235+holtz27@users.noreply.github.com>
2025-03-24 17:06:19 -0300

    Update waic.R

--- cobal/source/waic.R
+++ cobal/source/waic.R
@@ -1,21 +1,12 @@
 ############### waic
-waic = function(data, b, a, h, dyn=TRUE){
+waic = function(data, mu_t, h){
   n = length(data)
   M = matrix(nrow = dim(h)[1], ncol = n)
-  if(dyn){
-    for(j in 1:n){
-      M[, j] = dnorm(data[j], 
-                     mean = b + a[,j]*exp(h[,j]), 
-                     sd = exp(0.5*h[,j]), 
-                     log = TRUE)
-    }
-  }else{
-    for(j in 1:n){
-      M[, j] = dnorm(data[j], 
-                     mean = b + a*exp(h[,j]), 
-                     sd = exp(0.5*h[,j]), 
-                     log = TRUE)
-    }
+  for(j in 1:n){
+    M[, j] = dnorm(data[j], 
+                   mean = mu_t[,j], 
+                   sd = exp(0.5*h[,j]), 
+                   log = TRUE)
   }
   return(loo::waic(M))
 }
