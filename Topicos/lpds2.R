a29c61bfa0dc56e4d7948c28be7537b8f081132b
holtz27 <97683235+holtz27@users.noreply.github.com>
2025-04-29 17:54:32 -0300

    Create lpds2.R

---
+++ Topicos/lpds2.R
@@ -0,0 +1,39 @@
+
+
+lpds=function(ht, at, theta, yobs){
+  
+  rtnorm = function(n){
+    u = runif(n)
+    return(qnorm(0.5*(u+1)))
+  }
+  
+  mu=theta[1,]
+  phi=theta[2,]
+  sh=theta[3,]
+  sa=theta[4,]
+  v=theta[5,]
+  
+  k=length(yobs)
+  N=length(ht)
+  newy=matrix(0, N, k)
+  
+  for(j in 1:k){
+    newa=newW=newU=newh=delta=k1=k2=omega=gammat=mut=st=numeric(N)
+    for(i in 1:N){
+      newh[i] = mu[i] + phi[i]*(ht[i]-mu[i]) + sh[i]*rnorm(1)
+      newU[i] = rgamma(1, 0.5*v[i], 0.5*v[i])
+      newW[i] = rtnorm(1)
+      newa[i] = at[i] + sa[i]*rnorm(1)
+      
+      delta[i] = newa[i]/sqrt(1+newa[i]*newa[i])
+      k1[i] = sqrt(0.5*v[i])*gamma(0.5*(v[i]-1))/gamma(0.5*v[i])
+      k2[i] = v[i]/(v[i]-2)
+      omega[i] = 1/sqrt(k2[i]-(2/pi)*(delta[i]*k1[i])^2)
+      gammat[i] = -sqrt(2/pi)*delta[i]*omega[i]*k1[i]
+      mut[i] = gammat[i] + omega[i]*delta[i]*newW[i]*exp(0.5*newh[i])/sqrt(newU[i])
+      st[i] = omega[i]*sqrt(1-delta[i]^2)*exp(0.5*newh[i])/sqrt(newU[i])
+      newy[i, j] = mut[i] + st[i]*rnorm(1)
+    }
+  }
+  return(newy)
+}
