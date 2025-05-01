0d7f78ce22f29dba986a987b5f81ea4c75bdf7f5
holtz27 <97683235+holtz27@users.noreply.github.com>
2025-05-01 16:04:12 -0300

    Add files via upload

--- Topicos/analisys_results.R
+++ Topicos/analisys_results.R
@@ -1,27 +1,112 @@
-source('https://raw.githubusercontent.com/holtz27/Doutorado/refs/heads/main/source/res_sim.R')
-#load("~/Doutorado/Pesquisa/Papers/Topicos/DS-SV/Code/st/simulation/simulacao_sa_0.001.RData")
-#load("~/Doutorado/Pesquisa/Papers/Topicos/DS-SV/Code/st/simulation/simulacao_sa_0.05.RData")
-load("~/Doutorado/Pesquisa/Papers/Topicos/DS-SV/Code/st/simulation/simulacao_sa_0.1.RData")
 
-s1 = s2 = s3 = list()
-N = length( result )
-for(i in 1:N){
-  s1[[ i ]] = result[[ i ]]$summary1[ c(1:5, 8), ]
-  s2[[ i ]] = result[[ i ]]$summary2[ c(1:5, 8), ]
-  s3[[ i ]] = result[[ i ]]$summary3[ c(1:5, 8), ]
+### Exp
+#load("~/topicos/st/sim1/exp/0.1/exp_sa_0.1_1.RData")
+#res1=result
+#load("~/topicos/st/sim1/exp/0.1/exp_sa_0.1_2.RData")
+#res2=result
+### IG
+load("~/topicos/st/sim1/ig/0.05/ig_sa_0.05_1.RData")
+res1=result
+load("~/topicos/st/sim1/ig/0.05/ig_sa_0.05_2.RData")
+res2=result
+
+result = c(res1, res2)
+################################################################################
+its=length(result)
+errors=NULL
+for(i in 1:its){
+  
+  s=result[[i]][[1]]
+  if(sum(is.nan(s)+is.infinite(s))!= 0){
+    errors = cbind(errors, i) 
+  }
+  
+} 
+errors
+if(!is.null(errors)){
+  Res = result[-errors]
+}else{
+  Res = result
 } 
 
-# Parameter estimation
-res_sim( s1, theta_vdd, med.abs = TRUE )$metricas
-res_sim( s1, theta_vdd, med.abs = TRUE )$indx
+its=length(Res)
+M=result[[1]][[1]]
+Reps = matrix(0,nrow=dim(M)[1],ncol=its)
+for(i in 1:its){
+  Reps[,i] = Res[[i]][[1]][,1]
+}
 
-res_sim( s2, theta_vdd, med.abs = TRUE )$metricas
-res_sim( s2, theta_vdd, med.abs = TRUE )$indx
+# boxplot
+par(mfrow=c(1,5))
+boxplot(Reps[1,], main='mu')
+boxplot(Reps[2,], main='phi')
+boxplot(Reps[3,], main='sh')
+boxplot(Reps[4,], main='sa')
+boxplot(Reps[5,], main='nu')
+par(mfrow=c(1,1))
 
-res_sim( s3, theta_vdd, med.abs = TRUE )$metricas
-res_sim( s3, theta_vdd, med.abs = TRUE )$indx
 
-# resumo
-res_sim( s1, theta_vdd, med.abs = TRUE )$resumo
-res_sim( s2, theta_vdd, med.abs = TRUE )$resumo
-res_sim( s3, theta_vdd, med.abs = TRUE )$resumo
+indx=-1
+indx = cbind(indx, which(Reps[1,]>=3))
+#indx = cbind(indx, which(Reps[2,] <= 0.8))
+#indx = cbind(indx, which(Reps[3,] <= 0.8))
+#indx = cbind(indx, which(Reps[4,] <= 0.8))
+indx = cbind(indx, which(Reps[5,]>=30))
+indx = cbind(indx, which(Reps[5,]<=3))
+indx = unique(as.vector(indx))
+indx=indx[-1]
+indx
+
+Res[[indx[12]]]
+
+#L = result
+if(!is.null(indx)) Res=Res[-indx]
+its=length(Res)
+#M=L[[1]][[1]]
+Reps = matrix(0,nrow=dim(M)[1],ncol=its)
+for(i in 1:its){
+  Reps[,i] = Res[[i]][[1]][,1]
+}
+# boxplot
+par(mfrow=c(1,5))
+boxplot(Reps[1,], main='mu')
+boxplot(Reps[2,], main='phi')
+boxplot(Reps[3,], main='sh')
+boxplot(Reps[4,], main='sa')
+boxplot(Reps[5,], main='nu')
+par(mfrow=c(1,1))
+
+# error percent
+its
+(length(result)-its)/length(result)*100
+
+
+theta = theta_vdd
+theta[theta == 0] = 1
+m=dim(Reps)[1]
+n=dim(Reps)[2]
+err=min=max=cd=matrix(0,m,n)
+prob.cob = 0
+for(i in 1:n){
+  err[,i]=(Reps[,i]-theta_vdd)/theta
+  min[,i] = Res[[i]][[1]][,3]
+  max[,i] = Res[[i]][[1]][,5]
+  cd[,i] = Res[[i]][[1]][,'CD']
+  l1 = as.numeric(Res[[i]][[1]][,3] < theta_vdd)
+  l2 = as.numeric(Res[[i]][[1]][,5] > theta_vdd)
+  prob.piv = matrix(round(0.5*(l1+l2),0), ncol=1)
+  prob.cob = prob.cob + prob.piv
+}
+Data = cbind(theta_vdd,
+             matrix(apply(Reps, MARGIN=1,mean), ncol=1),
+             matrix(apply(min, MARGIN=1,mean), ncol=1),
+             matrix(apply(max, MARGIN=1,mean), ncol=1),
+             matrix(apply(cd, MARGIN=1,mean), ncol=1),
+             matrix(apply(err, MARGIN=1,mean), ncol=1),
+             matrix(sqrt(apply(err^2, MARGIN=1,mean)), ncol=1),
+             prob.cob/length(Res))
+row.names(Data) = c('mu', 'phi', 'sh', 'sa', 'v')
+colnames(Data) = c('Theta','mean','2.5%','97.5%','CD','bias', 'reqm', 'prob.cob')
+round(Data,4)
+
+
