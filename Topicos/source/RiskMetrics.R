d368b607c5bc550a8104d5ed64705ba71d4669a8
holtz27 <97683235+holtz27@users.noreply.github.com>
2025-05-06 14:37:34 -0300

    Update RiskMetrics.R

--- Topicos/source/RiskMetrics.R
+++ Topicos/source/RiskMetrics.R
@@ -1,4 +1,6 @@
-RiskMetrics=function(ht, at, theta, yobs, alpha=c(0.01,0.05)){
+RiskMetrics=function(ht, at, theta, yobs, alpha=c(0.01,0.05), model){
+  
+  # model=c(sn, st, ss) 
   
   rtnorm = function(n){
     u = runif(n)
@@ -9,7 +11,7 @@ RiskMetrics=function(ht, at, theta, yobs, alpha=c(0.01,0.05)){
   phi=theta[2,]
   sh=theta[3,]
   sa=theta[4,]  
-  v=theta[5,]
+  if(model != 'sn') v=theta[5,]
   
   N=length(ht)
   newy=newa=newW=newU=newh=delta=k1=k2=omega=gammat=mut=st=numeric(N)
@@ -17,13 +19,27 @@ RiskMetrics=function(ht, at, theta, yobs, alpha=c(0.01,0.05)){
   
   for(i in 1:N){
     newh[i] = mu[i] + phi[i]*(ht[i]-mu[i]) + sh[i]*rnorm(1)
-    newU[i] = rgamma(1, shape=0.5*v[i], rate=0.5*v[i])
     newW[i] = rtnorm(1)
     newa[i] = at[i] + sa[i]*rnorm(1)
-    
     delta[i] = newa[i]/sqrt(1+newa[i]*newa[i])
-    k1[i] = sqrt(0.5*v[i])*gamma(0.5*(v[i]-1))/gamma(0.5*v[i])
-    k2[i] = v[i]/(v[i]-2)
+    
+    
+    if(model=='st'){
+      newU[i] = rgamma(1, shape=0.5*v[i], rate=0.5*v[i])
+      k1[i] = sqrt(0.5*v[i])*gamma(0.5*(v[i]-1))/gamma(0.5*v[i])
+      k2[i] = v[i]/(v[i]-2)
+    }else{
+      if(model=='ss'){
+        newU[i] = rbeta(1, shape1=v[i], shape2=1)
+        k1[i] = 2*v[i]/(2*v[i]-1)
+        k2[i] = v[i]/(v[i]-1)
+      }else{
+        newU[i] = 1.0
+        k1[i] = 1.0
+        k2[i] = 1.0
+      }
+    }
+    
     omega[i] = 1/sqrt(k2[i]-(2/pi)*(delta[i]*k1[i])^2)
     gammat[i] = -sqrt(2/pi)*delta[i]*omega[i]*k1[i]
     mut[i] = gammat[i] + omega[i]*delta[i]*newW[i]*exp(0.5*newh[i])/sqrt(newU[i])
