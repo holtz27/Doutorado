4d578b255b1f92cbc88824a1ff2f74b6df82f66d
holtz27 <97683235+holtz27@users.noreply.github.com>
2025-05-19 16:53:38 -0300

    Update ISdiag.R

--- HMMcodes/source/ISdiag.R
+++ HMMcodes/source/ISdiag.R
@@ -13,62 +13,104 @@ quantile = function(x, weights, probs=c(0.025, 0.5, 0.975)){
   
   return(quantiles)
 }
+
 ISdiag = function(Weigth, X, nu.lower=0, nu.upper=Inf){
   
-  p <- function(parvect, nu.lower, nu.upper){
-    
-    # Inicializar beta
-    beta <- array(0, dim = 3)
-    beta[1] <- parvect[1]
-    if (parvect[2] > log_double_xmax) {
-      beta[2] <- 1.0 - double_eps
-    } else if (parvect[2] < -log_double_xmax) {
-      beta[2] <- -1.0 + double_eps
-    } else {
-      beta[2] <- (exp(parvect[2]) - 1) / (exp(parvect[2]) + 1)
-    }
-    beta[3] <- parvect[3]
-    
-    mu <- parvect[4]
-    if (parvect[5] > log_double_xmax) {
-      phi <- 1.0 - double_eps
-    } else if (parvect[5] < -log_double_xmax) {
-      phi <- -1.0 + double_eps
-    } else {
-      phi <- (exp(parvect[5]) - 1) / (exp(parvect[5]) + 1)
-    }
-    
-    if (parvect[6] > log_double_xmax) {
-      sigma <- double_xmax
-    } else if (parvect[6] < -log_double_xmax) {
-      sigma <- double_xmin
-    } else {
-      sigma <- exp(parvect[6])
-    }
-    
-    # Verificações para nu
-    if(is.finite(nu.upper)){
-      if (parvect[7] > log_double_xmax){
-        nu = nu.upper
-      } else if (parvect[7] < -log_double_xmax){
-        nu = nu.lower
+  if(nu.lower == Inf){
+    p <- function(parvect){
+      
+      # Inicializar beta
+      beta <- array(0, dim = 3)
+      beta[1] <- parvect[1]
+      if (parvect[2] > log_double_xmax) {
+        beta[2] <- 1.0 - double_eps
+      } else if (parvect[2] < -log_double_xmax) {
+        beta[2] <- -1.0 + double_eps
       } else {
-        nu = (nu.upper*exp(parvect[7])+nu.lower)/(1+exp(parvect[7]))
+        beta[2] <- (exp(parvect[2]) - 1) / (exp(parvect[2]) + 1)
       }
-    }else{
-      if (parvect[7] > log_double_xmax){
-        nu = double_xmax
-      } else if (parvect[7] < -log_double_xmax){
-        nu = double_xmin
+      beta[3] <- parvect[3]
+      
+      mu <- parvect[4]
+      if (parvect[5] > log_double_xmax) {
+        phi <- 1.0 - double_eps
+      } else if (parvect[5] < -log_double_xmax) {
+        phi <- -1.0 + double_eps
       } else {
-        nu = exp(parvect[7]) + nu.lower
+        phi <- (exp(parvect[5]) - 1) / (exp(parvect[5]) + 1)
       }
+      
+      if (parvect[6] > log_double_xmax) {
+        sigma <- double_xmax
+      } else if (parvect[6] < -log_double_xmax) {
+        sigma <- double_xmin
+      } else {
+        sigma <- exp(parvect[6])
+      }
+      
+      return(c(beta, mu, phi, sigma))
     }
-    
-    return(c(beta, mu, phi, sigma, nu))
+  }else{
+    p <- function(parvect, nu.lower, nu.upper){
+      
+      # Inicializar beta
+      beta <- array(0, dim = 3)
+      beta[1] <- parvect[1]
+      if (parvect[2] > log_double_xmax) {
+        beta[2] <- 1.0 - double_eps
+      } else if (parvect[2] < -log_double_xmax) {
+        beta[2] <- -1.0 + double_eps
+      } else {
+        beta[2] <- (exp(parvect[2]) - 1) / (exp(parvect[2]) + 1)
+      }
+      beta[3] <- parvect[3]
+      
+      mu <- parvect[4]
+      if (parvect[5] > log_double_xmax) {
+        phi <- 1.0 - double_eps
+      } else if (parvect[5] < -log_double_xmax) {
+        phi <- -1.0 + double_eps
+      } else {
+        phi <- (exp(parvect[5]) - 1) / (exp(parvect[5]) + 1)
+      }
+      
+      if (parvect[6] > log_double_xmax) {
+        sigma <- double_xmax
+      } else if (parvect[6] < -log_double_xmax) {
+        sigma <- double_xmin
+      } else {
+        sigma <- exp(parvect[6])
+      }
+      
+      # Verificações para nu
+      if(is.finite(nu.upper)){
+        if (parvect[7] > log_double_xmax){
+          nu = nu.upper
+        } else if (parvect[7] < -log_double_xmax){
+          nu = nu.lower
+        } else {
+          nu = (nu.upper*exp(parvect[7])+nu.lower)/(1+exp(parvect[7]))
+        }
+      }else{
+        if (parvect[7] > log_double_xmax){
+          nu = double_xmax
+        } else if (parvect[7] < -log_double_xmax){
+          nu = double_xmin
+        } else {
+          nu = exp(parvect[7]) + nu.lower
+        }
+      }
+      
+      return(c(beta, mu, phi, sigma, nu))
+    }
   }
+  
   ### mean #####################################################################
-  Thetas = t(apply(X=X, MARGIN=1, FUN=p, nu.lower=nu.lower, nu.upper=nu.upper))
+  if(nu.lower==Inf){
+    Thetas = t(apply(X=X, MARGIN=1, FUN=p))
+  }else{
+    Thetas = t(apply(X=X, MARGIN=1, FUN=p, nu.lower=nu.lower, nu.upper=nu.upper))
+  }
   wThetas = apply(X=Thetas, MARGIN = 2, FUN='*', Weigth)
   theta_hat = apply(X=wThetas, MARGIN = 2, sum)
   ### var ######################################################################
@@ -78,10 +120,13 @@ ISdiag = function(Weigth, X, nu.lower=0, nu.upper=Inf){
   Vars_hat = apply(X=wVars, MARGIN = 2, sum)
   ### quantiles ################################################################
   Quants = t(apply(Thetas, 2, quantile, Weigth))
-  
   Results = cbind(theta_hat, Vars_hat, Quants)
   colnames(Results) = c('mean', 'var', '2.5%', '50%', '97.5%')
-  row.names(Results) = c('b0','b1','b2','mu','phi','sigma','nu')
+  if(nu.lower==Inf){
+    row.names(Results) = c('b0','b1','b2','mu','phi','sigma')
+  }else{
+    row.names(Results) = c('b0','b1','b2','mu','phi','sigma','nu')
+  }
   Results = round(Results, 4)#signif(Results, 3)
   
   return(list(Results=Results, ess = diagis::ess(Weigth)))
