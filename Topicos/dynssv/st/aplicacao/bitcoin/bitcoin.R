d296ee18d268bb7aa88a9ed792d5a97593a1b756
holtz27 <97683235+holtz27@users.noreply.github.com>
2025-05-22 12:56:03 -0300

    Add files via upload

--- Topicos/dynssv/st/aplicacao/bitcoin/bitcoin.R
+++ Topicos/dynssv/st/aplicacao/bitcoin/bitcoin.R
@@ -1,7 +1,7 @@
 # Obtenha os dados do bitcoin
 bitcoin = quantmod::getSymbols('BTC-USD', 
                                src = 'yahoo', 
-                               from = '2017-01-07', to = '2022-06-12',
+                               from = '2015-04-07', to = '2020-05-01',
                                auto.assign = FALSE)
 bitcoin = na.omit(bitcoin)
 bitcoin = data.frame(bitcoin)
@@ -21,19 +21,33 @@ g = g + theme_test() + theme(axis.title.y=element_text(size=18),
                              axis.text.y=element_text(size=18))
 g = g + xlab('')
 h = ggplot(df, aes(Return))
-h = h + geom_histogram(aes(y = after_stat(density)), bins = 40, color = 'white')
+h = h + geom_histogram(aes(y = after_stat(density)), bins = 40, color='white')
 h = h + theme_test() + ylab('')
 h = h + theme_test() + theme(axis.title.x=element_text(size=18),
                              axis.text.x=element_text(size=12),
                              axis.text.y=element_text(size=18))
-gridExtra::grid.arrange(g, h, nrow = 1, ncol = 2) 
-data_summary = matrix(c( mean(log.ret),
-                         sd(log.ret),
-                         min(log.ret),
-                         max(log.ret),
-                         moments::skewness(log.ret),
-                         moments::kurtosis(log.ret)), nrow = 1)
+gridExtra::grid.arrange(g, h, nrow=1, ncol=2) 
+data_summary = matrix(c(mean(log.ret),
+                        sd(log.ret),
+                        min(log.ret),
+                        max(log.ret),
+                        moments::skewness(log.ret),
+                        moments::kurtosis(log.ret)), nrow=1)
 colnames(data_summary)=c('mean', 'sd', 'min', 'max', 'skewness', 'kurtosis')
 round(data_summary, digits=3)
 
 
+
+par(mfrow=c(1,2))
+y=log.ret-mean(log.ret)
+plot(y[1:(length(y)-120)], type='l')
+plot(tail(y, 120), type='l')
+par(mfrow=c(1,1))
+
+data_summary = matrix(c(mean(y[1:(length(y)-120)]),
+                        sd(y[1:(length(y)-120)]),
+                        min(y[1:(length(y)-120)]),
+                        max(y[1:(length(y)-120)]),
+                        moments::skewness(y[1:(length(y)-120)]),
+                        moments::kurtosis(y[1:(length(y)-120)])), nrow=1)
+round(data_summary, digits=3)
