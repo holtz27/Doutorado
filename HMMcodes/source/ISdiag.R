3e73f91c6a6f7e845fc975d9d11ab560eaadab46
holtz27 <97683235+holtz27@users.noreply.github.com>
2025-06-20 15:15:44 -0300

    Update ISdiag.R

--- HMMcodes/source/ISdiag.R
+++ HMMcodes/source/ISdiag.R
@@ -14,68 +14,12 @@ quant = function(x, weights, probs=c(0.025, 0.5, 0.975)){
   return(quants)
 }
 
-ISdiag = function(Weigth, X, nu.lower=0, nu.upper=Inf){
+ISdiag = function(Weigth, X, normal=FALSE){
   
-  if(nu.lower==Inf){
-    # svm-n
-    p=function(parvect){
-      
-      beta=array(0,dim=3)
-      beta[1]= parvect[1]
-      beta[2]=(exp(parvect[2])-1)/(exp(parvect[2])+1)
-      beta[3]=parvect[3]
-      mu=parvect[4]
-      phi = (exp(parvect[5])-1)/(exp(parvect[5])+1)
-      sigma = exp(parvect[6])
-      
-      return(c(beta, mu, phi, sigma))
-    }
-  }else{
-    if(nu.upper==Inf){
-      p=function(parvect, nu.lower){
-        
-        beta=array(0,dim=3)
-        beta[1]= parvect[1]
-        beta[2]=(exp(parvect[2])-1)/(exp(parvect[2])+1)
-        beta[3]=parvect[3]
-        mu=parvect[4]
-        phi = (exp(parvect[5])-1)/(exp(parvect[5])+1)
-        sigma = exp(parvect[6])
-        nu=exp(parvect[7])+nu.lower
-        
-        return(c(beta, mu, phi, sigma, nu))
-      }
-    }else{
-      p=function(parvect, nu.lower, nu.upper){
-        
-        beta=array(0,dim=3)
-        beta[1]= parvect[1]
-        beta[2]=(exp(parvect[2])-1)/(exp(parvect[2])+1)
-        beta[3]=parvect[3]
-        mu=parvect[4]
-        phi = (exp(parvect[5])-1)/(exp(parvect[5])+1)
-        sigma = exp(parvect[6])
-        
-        #nu = (40*exp(parvect[7])+2)/(1+exp(parvect[7]))
-        alpha=0.1
-        nu=0.5*((nu.upper-nu.lower)*tanh(0.5*alpha*parvect[7])+(nu.upper+nu.lower))
-        
-        return(c(beta, mu, phi, sigma, nu))
-      }
-    }
-    
-  }
+  p=get('svm.pw2pn', envir=.GlobalEnv)
   
   ### mean #####################################################################
-  if(nu.lower==Inf){
-    Thetas=t(apply(X=X, MARGIN=1, FUN=p))
-  }else{
-    if(nu.upper==Inf){
-      Thetas=t(apply(X=X, MARGIN=1, FUN=p, nu.lower=nu.lower))
-    }else{
-      Thetas=t(apply(X=X, MARGIN=1, FUN=p, nu.lower=nu.lower, nu.upper=nu.upper))  
-    }
-  }
+  Thetas=t(apply(X=X, MARGIN=1, FUN=p))
   wThetas = apply(X=Thetas, MARGIN = 2, FUN='*', Weigth)
   theta_hat = apply(X=wThetas, MARGIN = 2, sum)
   ### var ######################################################################
@@ -87,7 +31,7 @@ ISdiag = function(Weigth, X, nu.lower=0, nu.upper=Inf){
   Quants = t(apply(Thetas, 2, quant, Weigth))
   Results = cbind(theta_hat, Vars_hat, Quants)
   colnames(Results) = c('mean', 'var', '2.5%', '50%', '97.5%')
-  if(nu.lower==Inf){
+  if(normal){
     row.names(Results) = c('b0','b1','b2','mu','phi','sigma')
   }else{
     row.names(Results) = c('b0','b1','b2','mu','phi','sigma','nu')
