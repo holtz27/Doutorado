1d33a0182c8097849a25d648ab9b3106c0005478
holtz27 <97683235+holtz27@users.noreply.github.com>
2025-06-24 14:08:46 -0300

    Add files via upload

--- HMMcodes/Hmmnew/aplication/mexbol/mexbol.R
+++ HMMcodes/Hmmnew/aplication/mexbol/mexbol.R
@@ -40,7 +40,6 @@ data_summary=matrix(c(T, mean(ytrain),
                       moments::kurtosis(ytrain)), nrow=1)
 colnames(data_summary)=c('T','mean', 'sd', 'min', 'max', 'skewness', 'kurtosis')
 round(data_summary, digits=3)
-
 ################################################################################
 ####### Fitting
 ################################################################################
@@ -67,15 +66,17 @@ save(Results, times, h_hat, DIC, LPS, psressvmvg,
 ### Analysis Results
 ################################################################################
 # SVM-N
-load("~/Documentos/svmHMM/mexbol/n.RData")
+load("~/HMMnew/aplication/mexbol/n.RData")
 Results
 times
 DIC
 LPS
 
-plot(abs(ytrain), type='l', col='gray')
-lines(exp(0.5*h_hat), lwd=2)
-qqnorm(qnorm(psressvmn), main='SVM-N') 
+#plot(abs(ytrain), type='l', col='gray')
+#lines(exp(0.5*h_hat), lwd=2)
+nh_hat=h_hat
+
+qqnorm(qnorm(psressvmn), main='SVM-N', cex.main=2.5, cex.axis=1.8, cex.lab=1.8) 
 qqline(qnorm(psressvmn))
 require(tseries)
 pvalue=jarque.bera.test(qnorm(psressvmn))$p.value
@@ -93,84 +94,183 @@ row.names(VaR)=c('0.01', '0.05', '0.10', '0.50')
 colnames(VaR)=c('Observed', 'Expected')
 VaR
 
+###############################################################################
+# Unconditional Coverage Test 
+n=length(ytest)
+x=VaR[1,1]
+alpha=0.01
+alpha_hat=x/n
+LRuc=log(alpha_hat^{x}*(1-alpha_hat)^{n-x})
+LRuc=LRuc-log(alpha^{x}*(1-alpha)^{n-x})
+LRuc=2*LRuc
+pvalue001=pchisq(LRuc, df=1, lower.tail=FALSE)
 
+
+x=VaR[2,1]
+alpha=0.05
+alpha_hat=x/n
+LRuc=log(alpha_hat^{x}*(1-alpha_hat)^{n-x})
+LRuc=LRuc-log(alpha^{x}*(1-alpha)^{n-x})
+LRuc=2*LRuc
+pvalue005=pchisq(LRuc, df=1, lower.tail=FALSE)
+
+round(pvalue001, 4)
+round(pvalue005, 4)
 ################################################################################
 # SVM-t
-load("~/Documentos/svmHMM/mexbol/t.RData")
+load("~/HMMnew/aplication/mexbol/t.RData")
 Results
 times
 DIC
 LPS
-plot(abs(ytrain), type='l', col='gray')
-lines(exp(0.5*h_hat), lwd=2)
-qqnorm(qnorm(psressvmn), main='SVM-t') 
-qqline(qnorm(psressvmn))
+#plot(abs(ytrain), type='l', col='gray')
+#lines(exp(0.5*h_hat), lwd=2)
+th_hat=h_hat
+
+qqnorm(qnorm(psressvmt), main='SVM-t', cex.main=2.5, cex.axis=1.8, cex.lab=1.8) 
+qqline(qnorm(psressvmt))
 require(tseries)
-pvalue=jarque.bera.test(qnorm(psressvmn))$p.value
+pvalue=jarque.bera.test(qnorm(psressvmt))$p.value
 if(pvalue>0.05){
   cat('Not reject the hypothesis of normality of the residuals at the 5% nivel.', '\n')
 }else{
   cat('Reject the hypothesis of normality of the residuals at the 5% nivel.', '\n')
 }
 
-VaR=c(sum(psressvmn<0.01), 0.01*length(ytest))
-VaR=rbind(VaR, c(sum(psressvmn<0.05), 0.05*length(ytest)))
-VaR=rbind(VaR, c(sum(psressvmn<0.1), 0.1*length(ytest)))
-VaR=rbind(VaR, c(sum(psressvmn<0.5), 0.5*length(ytest)))
+VaR=c(sum(psressvmt<0.01), 0.01*length(ytest))
+VaR=rbind(VaR, c(sum(psressvmt<0.05), 0.05*length(ytest)))
+VaR=rbind(VaR, c(sum(psressvmt<0.1), 0.1*length(ytest)))
+VaR=rbind(VaR, c(sum(psressvmt<0.5), 0.5*length(ytest)))
 row.names(VaR)=c('0.01', '0.05', '0.10', '0.50')
 colnames(VaR)=c('Observed', 'Expected')
 VaR
+###############################################################################
+# Unconditional Coverage Test 
+n=length(ytest)
+x=VaR[1,1]
+alpha=0.01
+alpha_hat=x/n
+LRuc=log(alpha_hat^{x}*(1-alpha_hat)^{n-x})
+LRuc=LRuc-log(alpha^{x}*(1-alpha)^{n-x})
+LRuc=2*LRuc
+pvalue001=pchisq(LRuc, df=1, lower.tail=FALSE)
 
+
+x=VaR[2,1]
+alpha=0.05
+alpha_hat=x/n
+LRuc=log(alpha_hat^{x}*(1-alpha_hat)^{n-x})
+LRuc=LRuc-log(alpha^{x}*(1-alpha)^{n-x})
+LRuc=2*LRuc
+pvalue005=pchisq(LRuc, df=1, lower.tail=FALSE)
+
+round(pvalue001, 4)
+round(pvalue005, 4)
 ################################################################################
 # SVM-S
-load("~/Documentos/svmHMM/mexbol/s.RData")
+load("~/HMMnew/aplication/mexbol/s.RData")
 Results
 times
 DIC
 LPS
-plot(abs(ytrain), type='l', col='gray')
-lines(exp(0.5*h_hat), lwd=2)
-qqnorm(qnorm(psressvmn), main='SVM-S') 
-qqline(qnorm(psressvmn))
+#plot(abs(ytrain), type='l', col='gray')
+#lines(exp(0.5*h_hat), lwd=2)
+sh_hat=h_hat
+
+qqnorm(qnorm(psressvms), main='SVM-S', cex.main=2.5, cex.axis=1.8, cex.lab=1.8) 
+qqline(qnorm(psressvms))
 require(tseries)
-pvalue=jarque.bera.test(qnorm(psressvmn))$p.value
+pvalue=jarque.bera.test(qnorm(psressvms))$p.value
 if(pvalue>0.05){
   cat('Not reject the hypothesis of normality of the residuals at the 5% nivel.', '\n')
 }else{
   cat('Reject the hypothesis of normality of the residuals at the 5% nivel.', '\n')
 }
 
-VaR=c(sum(psressvmn<0.01), 0.01*length(ytest))
-VaR=rbind(VaR, c(sum(psressvmn<0.05), 0.05*length(ytest)))
-VaR=rbind(VaR, c(sum(psressvmn<0.1), 0.1*length(ytest)))
-VaR=rbind(VaR, c(sum(psressvmn<0.5), 0.5*length(ytest)))
+VaR=c(sum(psressvmt<0.01), 0.01*length(ytest))
+VaR=rbind(VaR, c(sum(psressvms<0.05), 0.05*length(ytest)))
+VaR=rbind(VaR, c(sum(psressvms<0.1), 0.1*length(ytest)))
+VaR=rbind(VaR, c(sum(psressvms<0.5), 0.5*length(ytest)))
 row.names(VaR)=c('0.01', '0.05', '0.10', '0.50')
 colnames(VaR)=c('Observed', 'Expected')
 VaR
+###############################################################################
+# Unconditional Coverage Test 
+n=length(ytest)
+x=VaR[1,1]
+alpha=0.01
+alpha_hat=x/n
+LRuc=log(alpha_hat^{x}*(1-alpha_hat)^{n-x})
+LRuc=LRuc-log(alpha^{x}*(1-alpha)^{n-x})
+LRuc=2*LRuc
+pvalue001=pchisq(LRuc, df=1, lower.tail=FALSE)
 
+
+x=VaR[2,1]
+alpha=0.05
+alpha_hat=x/n
+LRuc=log(alpha_hat^{x}*(1-alpha_hat)^{n-x})
+LRuc=LRuc-log(alpha^{x}*(1-alpha)^{n-x})
+LRuc=2*LRuc
+pvalue005=pchisq(LRuc, df=1, lower.tail=FALSE)
+
+round(pvalue001, 4)
+round(pvalue005, 4)
 ################################################################################
 # SVM-VG
-load("~/Documentos/svmHMM/mexbol/vg.RData")
+load("~/HMMnew/aplication/mexbol/vg.RData")
 Results
 times
 DIC
 LPS
-plot(abs(ytrain), type='l', col='gray')
-lines(exp(0.5*h_hat), lwd=2)
-qqnorm(qnorm(psressvmn), main='SVM-VG') 
-qqline(qnorm(psressvmn))
+#plot(abs(ytrain), type='l', col='gray')
+#lines(exp(0.5*h_hat), lwd=2)
+vgh_hat=h_hat
+
+qqnorm(qnorm(psressvmvg), main='SVM-VG', cex.main=2.5, cex.axis=1.8, cex.lab=1.8) 
+qqline(qnorm(psressvmvg))
 require(tseries)
-pvalue=jarque.bera.test(qnorm(psressvmn))$p.value
+pvalue=jarque.bera.test(qnorm(psressvmvg))$p.value
 if(pvalue>0.05){
   cat('Not reject the hypothesis of normality of the residuals at the 5% nivel.', '\n')
 }else{
   cat('Reject the hypothesis of normality of the residuals at the 5% nivel.', '\n')
 }
 
-VaR=c(sum(psressvmn<0.01), 0.01*length(ytest))
-VaR=rbind(VaR, c(sum(psressvmn<0.05), 0.05*length(ytest)))
-VaR=rbind(VaR, c(sum(psressvmn<0.1), 0.1*length(ytest)))
-VaR=rbind(VaR, c(sum(psressvmn<0.5), 0.5*length(ytest)))
+VaR=c(sum(psressvmvg<0.01), 0.01*length(ytest))
+VaR=rbind(VaR, c(sum(psressvmvg<0.05), 0.05*length(ytest)))
+VaR=rbind(VaR, c(sum(psressvmvg<0.1), 0.1*length(ytest)))
+VaR=rbind(VaR, c(sum(psressvmvg<0.5), 0.5*length(ytest)))
 row.names(VaR)=c('0.01', '0.05', '0.10', '0.50')
 colnames(VaR)=c('Observed', 'Expected')
 VaR
+###############################################################################
+# Unconditional Coverage Test 
+n=length(ytest)
+x=VaR[1,1]
+alpha=0.01
+alpha_hat=x/n
+LRuc=log(alpha_hat^{x}*(1-alpha_hat)^{n-x})
+LRuc=LRuc-log(alpha^{x}*(1-alpha)^{n-x})
+LRuc=2*LRuc
+pvalue001=pchisq(LRuc, df=1, lower.tail=FALSE)
+
+
+x=VaR[2,1]
+alpha=0.05
+alpha_hat=x/n
+LRuc=log(alpha_hat^{x}*(1-alpha_hat)^{n-x})
+LRuc=LRuc-log(alpha^{x}*(1-alpha)^{n-x})
+LRuc=2*LRuc
+pvalue005=pchisq(LRuc, df=1, lower.tail=FALSE)
+
+round(pvalue001, 4)
+round(pvalue005, 4)
+
+
+plot(abs(ytrain), type='l', col='gray', 
+     main='IPC MEXICO', xlab='', ylab='', cex.main=2.5, cex.axis=1.8, cex.lab=1.8)
+lines(exp(0.5*nh_hat), col='black', lwd=2)
+lines(exp(0.5*th_hat), col='red', lwd=2)
+lines(exp(0.5*sh_hat), col='orange', lwd=2)
+lines(exp(0.5*vgh_hat), col='purple', lwd=2)
