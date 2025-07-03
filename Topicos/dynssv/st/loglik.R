1b055c6da2c6f8e1af7ec3829775199618776c17
holtz27 <97683235+holtz27@users.noreply.github.com>
2025-07-03 18:14:36 -0300

    Add files via upload

---
+++ Topicos/dynssv/st/loglik.R
@@ -0,0 +1,214 @@
+loglik_APF = function(y, theta, N){
+  
+  moms = function(a, h, v){
+    
+    invsqrt_U=sqrt(0.5*v)*gamma(0.5*(v-1))/gamma(0.5*v)
+    #U=rgamma(length(a), shape=0.5*v, rate=0.5*v)
+    #invsqrt_U=1/sqrt(U)
+    W=sqrt(2/pi)
+    #W=rtnorm(length(a))
+    
+    delta_t = a / sqrt(1 + a^2)
+    k1=sqrt(0.5*v)*gamma(0.5*(v-1))/gamma(0.5*v)
+    k2=v/(v-2)
+    omega_t =1/sqrt(k2-(2/pi)*(delta_t*k1)^2)
+    gamma_t = -sqrt(2/pi)*delta_t*omega_t*k1
+    mu_t = gamma_t + omega_t * delta_t * W * exp(0.5*h) 
+    mu_t = mu_t * invsqrt_U
+    sigma_t = omega_t*sqrt(1-delta_t^2)*exp(0.5*h)
+    sigma_t = sigma_t * invsqrt_U
+    
+    return(matrix(c(mu_t, sigma_t), ncol=2))
+  }
+  
+  T = length(y)
+  mu = theta['mu']
+  phi = theta['phi']
+  sh = theta['sh']
+  sa = theta['sa']
+  v = theta['v']
+  
+  h_par = a_par = w = matrix(0, nrow = N, ncol = T)
+  
+  h_par0 = rnorm(N, mean=mu, sd=sh/sqrt(1-phi^2))
+  a_par0 = rep(0, N)
+  w0 = rep(1/N, N)
+  hbar=abar=Pk=indx=numeric(N)
+  
+  # t=1
+  # mu_t
+  hbar = mu + phi*(h_par0 - mu)
+  abar = a_par0
+  Ms = moms(a=abar, h=hbar, v=v)
+  Pk = dnorm(y[1], mean=Ms[,1], sd=Ms[,2]) * w0
+  Pk = Pk/sum(Pk)
+  indx = sample(1:N, N, prob=Pk, replace=TRUE)
+  
+  # new states
+  h_par[,1] = rnorm(N, mu + phi*(hbar[indx] - mu), sh)
+  a_par[,1] = rnorm(N, abar[indx], sa)
+  
+  Ms=moms(a=a_par[,1], h=h_par[,1], v=v)
+  dens1=dnorm(y[1], mean=Ms[,1], sd=Ms[,2])
+  
+  Ms=moms(a=abar, h=hbar, v=v)
+  dens2=dnorm(y[1], mean=Ms[,1], sd=Ms[,2])
+  
+  w[,1]=dens1/dens2
+  w[,1]=w[,1]/sum(w[,1])
+  
+  loglik = log( sum(dens1*w[,1]) )
+  
+  # t>1
+  for(t in 2:T){
+    
+    # mu_t
+    hbar = mu + phi*(h_par[,t-1] - mu)
+    abar = a_par[,t-1]
+    Ms=moms(a=abar, h=hbar, v=v)
+    Pk = dnorm(y[t], mean=Ms[,1], sd=Ms[,2]) * w[,t-1]
+    Pk = Pk/sum(Pk)
+    indx=sample(1:N, N, prob=Pk, replace=TRUE)
+    
+    # new states
+    h_par[,t] = rnorm(N, mu+phi*(hbar[indx]-mu), sh)
+    a_par[,t] = rnorm(N, abar[indx], sa)
+    
+    Ms=moms(a=a_par[,t], h=h_par[,t], v=v)
+    dens1=dnorm(y[t], mean=Ms[,1], sd=Ms[,2])
+    
+    Ms=moms(a=abar, h=hbar, v=v)
+    dens2=dnorm(y[t], mean=Ms[,1], sd=Ms[,2])
+    
+    w[,t]=dens1/dens2
+    w[,t]=w[,t]/sum(w[,t])
+    
+    loglik = loglik + log(sum( dens1*w[,t] ))
+    
+    hbar=abar=Pk=indx=numeric(N)
+  }
+  
+  return(list(loglik=loglik, pred_h=h_par, pred_a=a_par, w=w)) 
+}
+quant = function(x, weights, probs=c(0.025, 0.5, 0.975)){
+  
+  ix = order(x)
+  x_sorted = x[ix]
+  weights_sorted = weights[ix]
+  weights_sorted = weights_sorted / sum(weights_sorted)
+  cumulative_weights = cumsum(weights_sorted)
+  quants = numeric(length(probs))
+  for(i in seq_along(probs)){
+    index = which(cumulative_weights >= probs[i])[1]
+    quants[i] = x_sorted[index]
+  }
+  
+  return(quants)
+}
+################################################################################
+################################################################################
+
+Rcpp::sourceCpp('~/topicos/st/loglik_APF.cpp')
+
+
+
+
+
+
+theta=c(mu=mu, phi=phi_h, sh=s_h, sa=0.1, v=v)
+X=loglik_APF(y=y, theta=theta, N=1e4)
+X$loglik
+
+
+weigth=X$w
+plot(1/colSums( weigth^2 ))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+a_hat=a_min=a_max=numeric(T)
+h_hat=h_min=h_max=numeric(T)
+for(t in 1:T){
+  h_hat[t] = sum(X$pred_h[,t]*X$w[, t])
+  
+  a_min[t]=quant(X$pred_a[,t], weights=X$w[,t], probs=0.025)
+  a_hat[t]=sum(X$pred_a[,t]*X$w[,t])
+  a_max[t]=quant(X$pred_a[,t], weights=X$w[,t], probs=0.975)
+  
+}
+
+
+plot(h_hat, type='l', col='purple', lwd=1)
+lines(h, col='gray60', lwd=2)
+
+
+x=1:T
+plot(a, type='l', col='gray60', ylim=c(-1, 1), lwd=2)
+lines(a_hat, col='purple')
+lines(a_min, col='purple', lty=2)
+lines(a_max, col='purple', lty=2)
+
+polygon(c(x, rev(x)),               
+        c(a_max, rev(a_min)),       
+        col = rgb(0.6, 0.2, 0.8, 0.2), 
+        border = NA)                
+
+
+################################################################################
+################################################################################
+sax=seq(0, 1, 0.005)
+logL=numeric(length(sax))
+
+for(i in 1:length(sax)){
+  theta=c(mu=mu, phi=phi_h, sh=s_h, sa=sax[i], v=v)
+  X=loglik_APF(y=y, theta=theta, N=5e3)
+  logL[i]=X$loglik  
+}
+
+plot(sax, logL)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Topicos/dynssv/st/loglik_APF.cpp
@@ -0,0 +1,135 @@
+// [[Rcpp::depends(RcppArmadillo, Rcpp)]]
+
+#include <RcppArmadillo.h>
+
+using namespace Rcpp;
+using namespace arma;
+
+mat moms(vec a, vec h, double v, int N){
+  
+  mat M(N, 2);
+  
+  double invsqrt_U = sqrt(0.5*v)*tgamma(0.5*(v-1))/tgamma(0.5*v);
+  double W = sqrt(2/M_PI);
+  
+  
+  vec delta_t = a / sqrt(1 + pow(a, 2));
+  double k1 = sqrt(0.5*v)*tgamma(0.5*(v-1))/tgamma(0.5*v);
+  double k2 = v/(v-2);
+  vec omega_t = 1/sqrt(k2-(2/M_PI) * pow(k1*delta_t, 2) );
+  vec gamma_t = -sqrt(2/M_PI) * k1 * delta_t % omega_t;
+  vec mu_t = gamma_t + W * omega_t % delta_t % exp(0.5*h); 
+  mu_t *= invsqrt_U;
+  vec sigma_t = omega_t % sqrt(1-pow(delta_t, 2)) % exp(0.5*h);
+  sigma_t *= invsqrt_U;
+  
+  M.col(0) = mu_t;
+  M.col(1) = sigma_t;
+  
+  return M;
+}
+
+
+// [[Rcpp::export]]
+List loglik_APF(vec y, vec theta, int N){
+  
+  Function sample("sample.int");
+  
+  //arma::arma_rng::set_seed(42);
+  
+  int T = y.n_elem;
+  double mu = theta[0];
+  double phi = theta[1];
+  double sh = theta[2];
+  double sa = theta[3];
+  double v = theta[4]; 
+  
+  mat h_par(N, T);
+  mat a_par(N, T);
+  mat w(N, T);
+  
+  vec h_par0 = randn( N, distr_param(mu, sh/sqrt(1-phi*phi)) );
+  vec a_par0(N, fill::zeros);
+  vec w0(N, fill::ones);
+  w0 *= 1.0 / N;
+  
+  vec hbar(N), abar(N), pk(N);
+  
+  hbar = mu + phi*(h_par0 - mu);
+  abar = a_par0;
+  mat Ms = moms(abar, hbar, v, N);
+  
+  pk = normpdf(y(0), Ms.col(0), Ms.col(1)) % w0;
+  pk /= sum(pk);
+  
+  IntegerVector indx = sample(N, N, true, pk);
+  indx = as<arma::uvec>(indx);
+  
+  //new states
+  vec X(N);
+  for(int i=0; i < N; i++){
+    X(i) = mu + phi*( hbar(indx(i)-1)-mu );
+  }
+  //h_par.col(0) = mvnrnd(X, S);
+  h_par.col(0) = X + sh * randn(N);
+  
+  for(int i=0; i < N; i++){
+    X(i) = abar(indx(i)-1);
+  }
+  //a_par.col(0) = mvnrnd(X, S);
+  a_par.col(0) = X + sa * randn(N);
+  
+  Ms = moms(a_par.col(0), h_par.col(0), v, N);
+  vec dens1 = normpdf(y(0), Ms.col(0), Ms.col(1));
+    
+  Ms = moms(abar, hbar, v, N);
+  vec dens2 = normpdf(y(0), Ms.col(0), Ms.col(1));
+    
+  w.col(0) = dens1/dens2;
+  w.col(0) = w.col(0)/sum(w.col(0));
+
+  double loglik = log( sum( dens1 % w.col(0) ) );
+  
+  for(int t=1; t<T; t++){
+    
+    hbar = mu + phi*(h_par.col(t-1) - mu);
+    abar = a_par.col(t-1);
+    Ms = moms(abar, hbar, v, N);
+    
+    pk = normpdf(y(t), Ms.col(0), Ms.col(1)) % w.col(t-1);
+    pk /= sum(pk);
+    
+    indx = sample(N, N, true, pk);
+    indx = as<arma::uvec>(indx);
+    
+    //new states
+    for(int i=0; i < N; i++){
+      X(i) = mu + phi*( hbar(indx(i)-1)-mu );
+    }
+    h_par.col(t) = X + sh * randn(N);
+    
+
+    for(int i=0; i < N; i++){
+      X(i) = abar(indx(i)-1);
+    }
+    a_par.col(t) = X + sa * randn(N);
+    
+    Ms = moms(a_par.col(t), h_par.col(t), v, N);
+    dens1 = normpdf(y(t), Ms.col(0), Ms.col(1));
+    
+    Ms = moms(abar, hbar, v, N);
+    dens2 = normpdf(y(t), Ms.col(0), Ms.col(1));
+    
+    w.col(t) = dens1/dens2;
+    w.col(t) = w.col(t)/sum(w.col(t));
+    
+    loglik += log( sum( dens1 % w.col(t) ) );
+    
+  }
+  
+  return List::create(Named("loglik") = loglik,
+                      Named("pred_h") = h_par,
+                      Named("pred_a") = a_par,
+                      Named("w") = w);
+  
+}
